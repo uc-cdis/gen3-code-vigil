5c8fae056245722eefecccd81cea07f5fa9bc299
Hara Prasad Juvvala <haraprasadj@uchicago.edu>
2023-09-13 21:01:14 -0700

    fix

--- gen3-integration-tests/utils/gen3_ci/select_ci_environment.py
+++ gen3-integration-tests/utils/gen3_ci/select_ci_environment.py
@@ -51,5 +51,5 @@ if __name__ == "__main__":
 
     env_file = os.getenv("GITHUB_ENV")
     with open(env_file, "a") as myfile:
-        myfile.write(f"NAMESPACE={selected_ns}")
-        myfile.write(f"HOSTNAME={selected_ns}.planx-pla.net")
+        myfile.write(f'NAMESPACE="{selected_ns}"')
+        myfile.write(f'HOSTNAME="{selected_ns}.planx-pla.net"')
