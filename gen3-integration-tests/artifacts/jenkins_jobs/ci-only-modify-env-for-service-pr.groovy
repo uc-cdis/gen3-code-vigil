7527324987955f2defd2f9bc9171e40007aa816e
Hara Prasad Juvvala <haraprasadj@uchicago.edu>
2023-09-21 12:57:14 -0700

    add jenkins job

---
+++ gen3-integration-tests/artifacts/jenkins_jobs/ci-only-modify-env-for-service-pr.groovy
@@ -0,0 +1,68 @@
+/*
+    String parameter TARGET_ENVIRONMENT
+        e.g., qa-anvil
+    String parameter SERVICE
+        Key from the manifest's versions block
+        e.g., metadata
+    String parameter VERSION
+        Version, specifically the quay image tag
+        e.g., 2023.09
+*/
+pipeline {
+    agent {
+      node {
+        label 'gen3-qa-worker'
+      }
+    }
+    stages {
+        stage('Clean old workspace') {
+            steps {
+                cleanWs()
+            }
+        }
+        stage('Initial setup') {
+            steps {
+                // cloud-automation
+                checkout([
+                  $class: 'GitSCM',
+                  branches: [[name: 'refs/heads/master']],
+                  doGenerateSubmoduleConfigurations: false,
+                  extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'cloud-automation']],
+                  submoduleCfg: [],
+                  userRemoteConfigs: [[credentialsId: 'PlanXCyborgUserJenkins', url: 'https://github.com/uc-cdis/cloud-automation.git']]
+                ])
+                // gitops-qa
+                checkout([
+                  $class: 'GitSCM',
+                  branches: [[name: 'refs/heads/master']],
+                  doGenerateSubmoduleConfigurations: false,
+                  extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'cdis-manifest']],
+                  submoduleCfg: [],
+                  userRemoteConfigs: [[credentialsId: 'PlanXCyborgUserJenkins', url: 'https://github.com/uc-cdis/gitops-qa.git']]
+                ])
+            }
+        }
+        stage('Change service version and roll env') {
+            steps {
+                dir("cdis-manifest/$TARGET_ENVIRONMENT") {
+                    script {
+                        currentBranch = "${serviceName}:[a-zA-Z0-9._-]*"
+                        targetBranch = "${serviceName}:${quayBranchName}"
+                        echo "Editing cdis-manifest/${TARGET_ENVIRONMENT} service ${SERVICE} to version ${VERSION}"
+                        sh 'sed -i -e "s,'+"${currentBranch},${targetBranch}"+',g" manifest.json'
+                        sh 'cat manifest.json'
+                        sh '''#!/bin/bash +x
+                            set -e
+                            export GEN3_HOME=\$WORKSPACE/cloud-automation
+                            export KUBECTL_NAMESPACE=\${TARGET_ENVIRONMENT}
+                            source $GEN3_HOME/gen3/gen3setup.sh
+                            gen3 reset
+                            gen3 job run usersync
+                            g3kubectl wait --for=condition=complete --timeout=-1s jobs/usersync
+                        '''
+                    }
+                }
+            }
+        }
+    }
+}
--- gen3-integration-tests/utils/gen3_ci/prepare_ci_environment.py
+++ gen3-integration-tests/utils/gen3_ci/prepare_ci_environment.py
@@ -1,63 +1,53 @@
 import os
 import requests
-import sys
+import time
 
 from cdislogging import get_logger
+from datetime import datetime
 
 from utils.jenkins import JenkinsJob
 
 logger = get_logger(__name__, log_level=os.getenv("LOG_LEVEL", "info"))
 
 
-def wait_for_image_build(repo, branch):
-    branch = branch.replace("(", "_").replace(")", "_").replace("/", "_")
-    if os.getenv("QUAY_REPO"):
-        repo = os.getenv("QUAY_REPO")
+def wait_for_quay_build(repo, tag):
+    quay_url_org = "https://quay.io/api/v1/repository/cdis"
+    commit_time = datetime.strptime("os.getenv('COMMIT_TIME')", "%Y-%m-%dT%H:%M:%SZ")
+    max_tries = 30  # Minutes to wait for image
+    found = False
+    i = 0
+    logger.info(f"Repo - {repo}, image - {tag}")
+    while not found and i < max_tries:
+        logger.info(f"Waiting for image {repo}:{tag} to be built in quay")
+        res = requests.get(f"{quay_url_org}/{repo}/tag")
+        if res.status_code == 200:
+            branch_images = [x for x in res.json()["tags"] if x["name"] == tag]
+            if len(branch_images) >= 1:
+                image = branch_images[0]
+                image_time = datetime.utcfromtimestamp(image["start_ts"])
+                print(image_time)
+                if image_time > commit_time:
+                    found = True
+        i += 1
+        time.sleep(60)
+    if found:
+        return "success"
+    if not found:
+        logger.error(f"Image with tag {tag} was not found in repo {repo}")
+        return "failure"
 
 
-def prepare_ci_environment(namespace):
-    repo = os.getenv("REPO")
-
-    if repo in ("gen3-code-vigl", "gen3-qa"):  # Test repos
-        pass
-    elif repo in ("cdis-manifest", "gitops-qa"):  # Manifest repos
-        pass
-    else:  # Service repos
-        pass
-
-    # Roll all pods
+def modify_env_for_service_pr(namespace, service, tag):
     job = JenkinsJob(
         os.getenv("JENKINS_URL"),
         os.getenv("JENKINS_USERNAME"),
         os.getenv("JENKINS_PASSWORD"),
-        "ci-only-run-gen3-command",
+        "ci-only-modify-env-for-service-pr",
     )
     params = {
         "TARGET_ENVIRONMENT": namespace,
-        "COMMAND": "gen3 reset",
-    }
-    build_num = job.build_job(params)
-    if build_num:
-        status = job.wait_for_build_completion(build_num, max_duration=1800)
-        if status == "Completed":
-            return job.get_build_result(build_num)
-        else:
-            logger.error("Build timed out. Consider increasing max_duration")
-            return None
-    else:
-        logger.error("Build number not found")
-        return None
-
-    # Run usersync
-    job = JenkinsJob(
-        os.getenv("JENKINS_URL"),
-        os.getenv("JENKINS_USERNAME"),
-        os.getenv("JENKINS_PASSWORD"),
-        "ci-only-run-gen3-job",
-    )
-    params = {
-        "TARGET_ENVIRONMENT": namespace,
-        "JOB_NAME": "usersync",
+        "SERVICE": service,
+        "TAG": tag,
     }
     build_num = job.build_job(params)
     if build_num:
@@ -66,10 +56,27 @@ def prepare_ci_environment(namespace):
             return job.get_build_result(build_num)
         else:
             logger.error("Build timed out. Consider increasing max_duration")
-            return None
+            return "failure"
     else:
         logger.error("Build number not found")
-        return None
+        return "failure"
+
+
+def prepare_ci_environment(namespace):
+    repo = os.getenv("REPO")
+    # if quay repo name is different from github repo name
+    if os.getenv("QUAY_REPO"):
+        quay_repo = os.getenv("QUAY_REPO")
+    # quay image tag
+    quay_tag = os.getenv("REPO").replace("(", "_").replace(")", "_").replace("/", "_")
+    if repo in ("gen3-code-vigl", "gen3-qa"):  # Test repos
+        pass
+    elif repo in ("cdis-manifest", "gitops-qa"):  # Manifest repos
+        pass
+    else:  # Service repos
+        result = wait_for_quay_build(quay_repo, quay_tag)
+        assert result.lower() == "success"
+        result = modify_env_for_service_pr(quay_repo, quay_tag, namespace)
 
 
 if __name__ == "__main__":
