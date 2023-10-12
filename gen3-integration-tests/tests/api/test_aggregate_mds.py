99a04dd814f03c26b6aa0ee10518a28d514e12a1
Hara Prasad Juvvala <haraprasadj@uchicago.edu>
2023-10-12 15:54:41 -0700

    fix

--- gen3-integration-tests/tests/api/test_aggregate_mds.py
+++ gen3-integration-tests/tests/api/test_aggregate_mds.py
@@ -16,6 +16,9 @@ class TestAggregateMDS:
     def test_expect_pass(self):
         assert 1 == 1
 
+    def test_expect_pass_again(self):
+        assert 2 == 2
+
     def test_expect_fail(self):
         assert 1 == 2
 
