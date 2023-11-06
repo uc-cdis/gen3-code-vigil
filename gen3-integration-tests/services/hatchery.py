9ce7168ac56ff15b8fb2a0c82978eb9592964419
Atharva Rane <atharvar@uchicago.edu>
2023-11-06 15:06:56 -0500

    add hatchery UI tests

--- gen3-integration-tests/services/hatchery.py
+++ gen3-integration-tests/services/hatchery.py
@@ -1,6 +1,58 @@
 import pytest
+from playwright.sync_api import Page, expect
 
 
 class Hatchery(object):
     def __init__(self):
-        self.BASE_URL = f"{pytest.root_url}/explorer"
+        self.BASE_URL = f"{pytest.root_url}"
+        self.WORKSPACE_TAB = f"{self.BASE_URL}/workspace"
+
+    def go_to_workspace_page(self, page: Page):
+        """Goes to workspace page and checks if loaded correctly"""
+        page.goto(self.WORKSPACE_TAB)
+        workspace_page = page.locator(".workspace")
+        expect(workspace_page).to_be_visible
+
+    def open_jupyter_workspace(self, page: Page):
+        """Launch a jupyter workspace"""
+        workspace_options = page.locator(".workspace_options")
+        expect(workspace_options).to_be_visible
+        # find the workspace option for Juptyer WS to launch
+        generic_card = page.locator("//div[contains(text(), /^(Generic)/)]")
+        # click the launch button from the juptyer workspace card
+        launch_button = generic_card.get_by_role("button", name="Launch")
+        launch_button.click()
+
+    def open_python_kernel(self, page: Page):
+        """perform drs pull in workspace page"""
+        workspace_iframe = page.frame_locator('iframe[title="Workspace"]')
+        expect(workspace_iframe).to_be_visible
+        python_kernel_nb = (
+            page.frame_locator('iframe[title="Workspace"]')
+            .get_by_title("Python 3 (ipykernel)")
+            .first
+        )
+        python_kernel_nb.click()
+        expect(
+            page.frame_locator('iframe[title="Workspace"]').get_by_label(
+                "notebook content"
+            )
+        ).to_be_visible
+
+    def run_command_notebook(self, page: Page):
+        fill_command = (
+            page.frame_locator('iframe[title="Workspace"]')
+            .get_by_label("notebook content")
+            .locator("pre")
+        )
+        fill_command.click()
+        page.frame_locator('iframe[title="Workspace"]').get_by_label(
+            "notebook content"
+        ).get_by_role("textbox").fill("!gen3 drs-pull manifest <>manifest-name")
+        page.frame_locator('iframe[title="Workspace"]').get_by_role(
+            "button", name="Run the selected cells and advance"
+        ).click()
+
+    def terminate_workspace(self, page: Page):
+        page.get_by_role("button", name="Terminate Workspace").click()
+        page.get_by_role("button", name="Yes").click()
--- gen3-integration-tests/services/login.py
+++ gen3-integration-tests/services/login.py
@@ -1,19 +1,26 @@
+# Login Page:
+# functions to login and log out on the environment
+
 import re
 import pytest
 from playwright.sync_api import Page, expect
 
 
-class Homepage(object):
+class Login(object):
     def __init__(self):
         self.BASE_URL = f"{pytest.root_url}"
         self.LOGIN_URL = f"{self.BASE_URL}/login"
 
     def go_to_login_page(self, page: Page):
+        """Goes to the login page"""
         page.goto(self.LOGIN_URL)
         nav_bar = page.locator("css=nav_bar")
         expect(nav_bar).to_be_visible
 
     def user_login(self, page: Page, user="main_account"):
+        """Sets up Dev Cookie for main Account and logs in with Google
+        Also checks if the access_token exists after login
+        """
         page.context.add_cookies([{"name": "dev_login", "value": pytest.users[user]}])
         login_button = page.get_by_role(
             "button", name=re.compile("Google", re.IGNORECASE)
@@ -22,10 +29,17 @@ class Homepage(object):
         login_button.click()
         username = page.wait_for_selector("//div[@class='top-bar']//a[3]")
         assert (username.text) == pytest.users[user]
+        cookies = page.context.cookies()
+        for cookie in cookies:
+            if cookie["name"] == "access_token":
+                break
 
     def user_logout(self, page: Page):
+        """Logs out and wait for Login button on nav bar"""
         page.get_by_role("button", name="Logout").click()
         nav_bar_login_button = page.get_by_role("button", name="Login")
         expect(nav_bar_login_button).to_be_visible
 
     # function to handle pop ups after login
+    # def handle_popup(self, page: Page):
+    #     """Handling popups after login"""
