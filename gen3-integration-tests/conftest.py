dc18def6c1a2386b5b6769eea80d401695349ebe
Alex VanTol <avantol@uchicago.edu>
2024-01-05 14:47:39 -0600

    chore(debug): more debug info when API/access token fails

--- gen3-integration-tests/conftest.py
+++ gen3-integration-tests/conftest.py
@@ -44,7 +44,11 @@ def pytest_configure(config):
     )
     pytest.api_key_ids.append(api_key_json["key_id"])
     api_key = api_key_json["api_key"]
-    access_token = fence.get_access_token(api_key)
+    try:
+        access_token = fence.get_access_token(api_key)
+    except Exception as exc:
+        print(f"Failed to get access token using API Key: {file_path}")
+        raise
     pytest.auth_headers["main_account"] = {
         "Accept": "application/json",
         "Authorization": f"bearer {access_token}",
@@ -58,7 +62,11 @@ def pytest_configure(config):
     )
     pytest.api_key_ids.append(api_key_json["key_id"])
     api_key = api_key_json["api_key"]
-    access_token = fence.get_access_token(api_key)
+    try:
+        access_token = fence.get_access_token(api_key)
+    except Exception as exc:
+        print(f"Failed to get access token using API Key: {file_path}")
+        raise
     pytest.auth_headers["indexing_account"] = {
         "Accept": "application/json",
         "Authorization": f"bearer {access_token}",
