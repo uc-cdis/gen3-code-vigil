uc-cdis/gen3-code-vigil

commit f88b2094181d093e051094ff638123478c0890d1

Fetch manifest.json + more error handling (#22)

gen3-integration-tests/gen3_ci/jenkins_jobs/ci-only-select-and-lock-namespace.groovy
@@ -39,7 +39,7 @@ spec:
   - name: wait-for-jenkins-connection
     image: quay.io/cdis/gen3-ci-worker:master
     command: ["/bin/sh","-c"]
-    args: ["while [ $(curl -sw '%{http_code}' http://jenkins-master-service:8080/tcpSlaveAgentListener/ -o /dev/null) -ne 200 ]; do sleep 5; echo 'Waiting for jenkins connection ...'; done"]
+    args: ["while [ $(curl -sw '%{http_code}' http://jenkins-master-service:8080/tcpSlaveAgentListener/ -o /dev/null) -ne 200 ]; do sleep 5; echo 'Waiting for jenkins connection...'; done"]
   containers:
   - name: jnlp
     command: ["/bin/sh","-c"]
@@ -106,6 +106,7 @@ spec:
                 dir("select-and-lock-namespace") {
                     script {
                         sh '''#!/bin/bash +x
+                            set -e
                             export GEN3_HOME=\$WORKSPACE/cloud-automation
                             source \$GEN3_HOME/gen3/gen3setup.sh
                             times=0

.github/workflows/shared_integration_tests.yaml
@@ -163,7 +163,8 @@ jobs:
 
           - name: Archive pod logs from CI environment
             id: archive_pod_logs
-            if: ${{ steps.prep_ci_env.outcome == 'success' || steps.prep_ci_env.outcome == 'failure' }}
+            # if: ${{ steps.prep_ci_env.outcome == 'success' || steps.prep_ci_env.outcome == 'failure' }}
+            if: ${{ always() }}
             run: poetry run python -m gen3_ci.scripts.save_ci_env_pod_logs
 
           - name: Publish pod logs url to the PR
@@ -175,7 +176,8 @@ jobs:
 
           - name: Release CI environment
             id: release_ci_env
-            if: ${{ env.NAMESPACE != 'None' }}
+            # if: ${{ env.NAMESPACE != 'None' }}
+            if: ${{ always() }}
             run: poetry run python -m gen3_ci.scripts.release_ci_environment
 
           - name: Mark workflow as failed for unsuccessful test runs

.gitignore
@@ -3,4 +3,6 @@ __pycache__
 *.pyc
 .env
 .pytest_cache
+
 output/
+gen3-integration-tests/test_data/configuration

gen3-integration-tests/conftest.py
@@ -5,6 +5,7 @@ import pytest
 from pathlib import Path
 
 from services.fence import Fence
+from utils import gen3_admin_tasks
 
 # Using dotenv to simplify setting up env vars locally
 from dotenv import load_dotenv
@@ -39,31 +40,57 @@ def pytest_configure(config):
     # Save API key id's for cleanup
     pytest.api_key_ids = []
     # Default user - main_account - cdis.autotest@gmail.com
-    api_key_json = json.loads(
-        (Path.home() / ".gen3" / f"{pytest.namespace}_main_account.json").read_text()
-    )
+    file_path = Path.home() / ".gen3" / f"{pytest.namespace}_main_account.json"
+    try:
+        api_key_json = json.loads(file_path.read_text())
+    except FileNotFoundError:
+        print(f"API key file not found: '{file_path}'")
+        raise
     pytest.api_key_ids.append(api_key_json["key_id"])
     api_key = api_key_json["api_key"]
-    access_token = fence.get_access_token(api_key)
+    try:
+        access_token = fence.get_access_token(api_key)
+    except Exception as exc:
+        print(f"Failed to get access token using API Key: {file_path}")
+        raise
     pytest.auth_headers["main_account"] = {
         "Accept": "application/json",
         "Authorization": f"bearer {access_token}",
         "Content-Type": "application/json",
     }
     # Indexing admin - indexing_account - ctds.indexing.test@gmail.com
-    api_key_json = json.loads(
-        (
-            Path.home() / ".gen3" / f"{pytest.namespace}_indexing_account.json"
-        ).read_text()
-    )
+    file_path = Path.home() / ".gen3" / f"{pytest.namespace}_indexing_account.json"
+    try:
+        api_key_json = json.loads(file_path.read_text())
+    except FileNotFoundError:
+        print(f"API key file not found: '{file_path}'")
+        raise
     pytest.api_key_ids.append(api_key_json["key_id"])
     api_key = api_key_json["api_key"]
-    access_token = fence.get_access_token(api_key)
+    try:
+        access_token = fence.get_access_token(api_key)
+    except Exception as exc:
+        print(f"Failed to get access token using API Key: {file_path}")
+        raise
     pytest.auth_headers["indexing_account"] = {
         "Accept": "application/json",
         "Authorization": f"bearer {access_token}",
         "Content-Type": "application/json",
     }
+
+
+@pytest.fixture(autouse=True, scope="session")
+def get_configuration_files():
+    """
+    Get configuration files from the admin VM and save them at `test_data/configuration`
+    """
+    print("Creating configuration files")
+    configs = gen3_admin_tasks.get_admin_vm_configurations(pytest.namespace)
+    path = Path(__file__).parent / "test_data/configuration"
+    path.mkdir(parents=True, exist_ok=True)
+    for file_name, contents in configs.items():
+        with (path / file_name).open("w", encoding="utf-8") as f:
+            f.write(contents)
 
 
 @pytest.fixture

gen3-integration-tests/gen3_ci/jenkins_jobs/ci-only-fetch-configs.groovy
@@ -2,7 +2,7 @@
   String parameter TARGET_ENVIRONMENT
     e.g., qa-anvil
 
-  Archived artifact - gitops.json
+  Archived artifacts - gitops.json, manifest.json
 */
 pipeline {
     agent {
@@ -34,6 +34,7 @@ pipeline {
                 dir("fetch-portal-config") {
                     script {
                         sh '''#!/bin/bash +x
+                            set -e
                             export GEN3_HOME=\$WORKSPACE/cloud-automation
                             export KUBECTL_NAMESPACE=\${TARGET_ENVIRONMENT}
                             source \$GEN3_HOME/gen3/gen3setup.sh
@@ -44,10 +45,27 @@ pipeline {
                 }
             }
         }
+        stage('Fetch manifest') {
+            steps {
+                dir("fetch-manifest") {
+                    script {
+                        sh '''#!/bin/bash +x
+                            set -e
+                            export GEN3_HOME=\$WORKSPACE/cloud-automation
+                            export KUBECTL_NAMESPACE=\${TARGET_ENVIRONMENT}
+                            source \$GEN3_HOME/gen3/gen3setup.sh
+                            RESULT=`g3kubectl get configmaps manifest-global -o json`
+                            echo "\$RESULT" > manifest.json
+                        '''
+                    }
+                }
+            }
+        }
     }
     post {
         always {
             archiveArtifacts artifacts: 'fetch-portal-config/gitops.json'
+            archiveArtifacts artifacts: 'fetch-manifest/manifest.json'
         }
     }
 }

gen3-integration-tests/gen3_ci/jenkins_jobs/ci-only-generate-api-key.groovy
@@ -38,7 +38,7 @@ spec:
   - name: wait-for-jenkins-connection
     image: quay.io/cdis/gen3-ci-worker:master
     command: ["/bin/sh","-c"]
-    args: ["while [ $(curl -sw '%{http_code}' http://jenkins-master-service:8080/tcpSlaveAgentListener/ -o /dev/null) -ne 200 ]; do sleep 5; echo 'Waiting for jenkins connection ...'; done"]
+    args: ["while [ $(curl -sw '%{http_code}' http://jenkins-master-service:8080/tcpSlaveAgentListener/ -o /dev/null) -ne 200 ]; do sleep 5; echo 'Waiting for jenkins connection...'; done"]
   containers:
   - name: jnlp
     command: ["/bin/sh","-c"]

gen3-integration-tests/gen3_ci/jenkins_jobs/ci-only-modify-env-for-service-pr.groovy
@@ -39,7 +39,7 @@ spec:
   - name: wait-for-jenkins-connection
     image: quay.io/cdis/gen3-ci-worker:master
     command: ["/bin/sh","-c"]
-    args: ["while [ $(curl -sw '%{http_code}' http://jenkins-master-service:8080/tcpSlaveAgentListener/ -o /dev/null) -ne 200 ]; do sleep 5; echo 'Waiting for jenkins connection ...'; done"]
+    args: ["while [ $(curl -sw '%{http_code}' http://jenkins-master-service:8080/tcpSlaveAgentListener/ -o /dev/null) -ne 200 ]; do sleep 5; echo 'Waiting for jenkins connection...'; done"]
   containers:
   - name: jnlp
     command: ["/bin/sh","-c"]
@@ -90,6 +90,12 @@ spec:
         }
         stage('Initial setup') {
             steps {
+                script {
+                    sh '''#!/bin/bash +x
+                        set -e
+                        echo TARGET_ENVIRONMENT: $TARGET_ENVIRONMENT
+                    '''
+                }
                 // cloud-automation
                 checkout([
                   $class: 'GitSCM',

gen3-integration-tests/gen3_ci/jenkins_jobs/ci-only-modify-env-for-test-repo-pr.groovy
@@ -33,7 +33,7 @@ spec:
   - name: wait-for-jenkins-connection
     image: quay.io/cdis/gen3-ci-worker:master
     command: ["/bin/sh","-c"]
-    args: ["while [ $(curl -sw '%{http_code}' http://jenkins-master-service:8080/tcpSlaveAgentListener/ -o /dev/null) -ne 200 ]; do sleep 5; echo 'Waiting for jenkins connection ...'; done"]
+    args: ["while [ $(curl -sw '%{http_code}' http://jenkins-master-service:8080/tcpSlaveAgentListener/ -o /dev/null) -ne 200 ]; do sleep 5; echo 'Waiting for jenkins connection...'; done"]
   containers:
   - name: jnlp
     command: ["/bin/sh","-c"]
@@ -84,6 +84,12 @@ spec:
         }
         stage('Initial setup') {
             steps {
+                script {
+                    sh '''#!/bin/bash +x
+                        set -e
+                        echo TARGET_ENVIRONMENT: $TARGET_ENVIRONMENT
+                    '''
+                }
                 // cloud-automation
                 checkout([
                   $class: 'GitSCM',

commit 97033fd3e3b6f9b06bbdf22afa3c5da5c5a28362

fix 'ci-only-select-and-lock-namespace' bug introduced in #22

gen3-integration-tests/gen3_ci/jenkins_jobs/ci-only-select-and-lock-namespace.groovy
@@ -106,7 +106,6 @@ spec:
                 dir("select-and-lock-namespace") {
                     script {
                         sh '''#!/bin/bash +x
-                            set -e
                             export GEN3_HOME=\$WORKSPACE/cloud-automation
                             source \$GEN3_HOME/gen3/gen3setup.sh
                             times=0