b1679d2dae3c1ca90f85ab86da05035aef39e793
Atharva Rane <atharvar@uchicago.edu>
2024-05-30 16:05:23 -0400

    add tests

---
+++ gen3-integration-tests/gen3_ci/jenkins_jobs/check-kube-pod.groovy
@@ -0,0 +1,97 @@
+/*
+    String parameter JOB_NAME
+    eg - manifest-indexing
+
+    String parameter LABEL_NAME
+    eg - sowerjob
+
+    String parameter IGNORE_FAILURE
+    default value - False
+*/
+pipeline {
+    agent {
+        node {
+            label 'gen3-ci-worker'
+        }
+    }
+    stages {
+        stage('Clean old workspace') {
+            steps {
+                cleanWs()
+            }
+        }
+        stage('Initial Setup') {
+            steps {
+                // cloud-automation
+                checkout([
+                  $class: 'GitSCM',
+                  branches: [[name: 'refs/heads/master']],
+                  doGenerateSubmoduleConfigurations: false,
+                  extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'cloud-automation']],
+                  submoduleCfg: [],
+                  userRemoteConfigs: [[credentialsId: 'PlanXCyborgUserJenkins', url: 'https://github.com/uc-cdis/cloud-automation.git']]
+                ])
+            }
+        }
+        stage('Check Kube Pod') {
+            options {
+                    timeout(time: 5, unit: 'MINUTES')   // timeout on this stage
+                }
+            steps {
+                dir("check-kube-pod"){
+                    script {
+                        try {
+                            sh '''#!/bin/bash +x
+                            set -e
+                            export GEN3_HOME=\$WORKSPACE/cloud-automation
+                            export KUBECTL_NAMESPACE=\${NAMESPACE}
+                            source $GEN3_HOME/gen3/gen3setup.sh
+
+                            N_ATTEMPTS=10
+                            echo "NAMESPACE : $KUBECTL_NAMESPACE"
+                            for (( i=1; i<=N_ATTEMPTS; i++ )); do
+                                sleep 10
+                                echo "Waiting for $JOBNAME job pod ..."
+
+                                # checking if there are pods with labelname mentioned in parameters
+                                podNames=$(g3kubectl -n $KUBECTL_NAMESPACE get pod --sort-by=.metadata.creationTimestamp -l app=$LABELNAME -o jsonpath="{.items[*].metadata.name}")
+                                if [[ -z "$podNames" ]]; then
+                                    echo "No pods found with label $LABELNAME"
+                                else
+                                    # if pod/s found, get the status of the latest pod
+                                    latestPodName=$(echo "$podNames" | awk '{print $NF}')
+                                    echo "Pod found with label $LABELNAME: $latestPodName"
+                                    podStatus=$(g3kubectl -n $KUBECTL_NAMESPACE get pod $latestPodName -o jsonpath='{.status.phase}')
+
+                                    if [ "$podStatus" == "Succeeded" ]; then
+                                        echo "The container from pod $latestPodName is ready! Proceed with the assertion checks..."
+                                        break
+                                    elif [ "$podStatus" == "Failed" ]; then
+                                        if [ "$IGNOREFAILURE" == "True" ]; then
+                                            echo "The container from pod $latestPodName failed as expected! Just ignore as this is part of a negative test."
+                                            break
+                                        else
+                                            echo "THE POD FAILED ON ATTEMPT $i. GIVING UP."
+                                            pod_logs=$(kubectl logs $latestPodName -n $NAMESPACE)
+                                            echo "Logs:\n$pod_logs"
+                                            exit 1
+                                        fi
+                                    fi
+                                fi
+                                if [ $i -eq $N_ATTEMPTS ]; then
+                                    echo "Max number of attempts reached: $i. Test has failed."
+                                    exit 1
+                                fi
+                            done
+                            '''
+                        }
+                        catch (ex) {
+                            metricsHelper.writeMetricWithResult(STAGE_NAME, false)
+                            pipelineHelper.handleError(e)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- gen3-integration-tests/tests/test_indexing_page.py
+++ gen3-integration-tests/tests/test_indexing_page.py
@@ -1,5 +1,12 @@
 import pytest
+import requests
 import os
+import utils.gen3_admin_tasks as gat
+
+from pages.login import LoginPage
+from pages.indexing_page import IndexingPage
+from services.indexd import Indexd
+from playwright.sync_api import expect
 
 from cdislogging import get_logger
 
@@ -7,20 +14,44 @@ logger = get_logger(__name__, log_level=os.getenv("LOG LEVEL", "info"))
 
 
 @pytest.mark.portal
+@pytest.mark.sower
+@pytest.mark.ssjdispatcher
 class TestIndexingPage:
-    def test_indexing_upload(self):
+    test_guid = "c2da639f-aa25-4c4d-8e89-02a143788268"
+    test_hash = "73d643ec3f4beb9020eef0beed440ad4"  # pragma: allowlist secret
+    expected_result = (
+        f"{test_guid},s3://cdis-presigned-url-test/testdata,,jenkins2,{test_hash},13,"
+    )
+
+    def test_indexing_upload_valid_manifest(self, page):
         """
         Scenario: Login and navigate to indexing page and upload dummy manifest
         Steps:
             1. Login with indexing_account user
             2. Navigate to indexing page
-            3. Upload dummy manifest and wait for manifest-indexing sower job to finish
+            3. Upload valid manifest and wait for manifest-indexing sower job to finish
             4. Check if the indexd record exists
-            5. Chekc if the indexd record hashes exists
+            5. Check if the indexd record hashes exists
         """
-        pass
+        login_page = LoginPage()
+        indexing_page = IndexingPage()
+        indexd = Indexd()
+        # Go to login page and login with indexing_account user
+        login_page.go_to(page)
+        login_page.login(page, user="indexing_account")
+        # Go to indexing page and validate page is loaded
+        indexing_page.go_to(page)
+        # Upload the valid manifest via indexing page
+        indexing_page.upload_valid_indexing_manifest(page)
+        # Check if the sowerjob pod for manifest-indexing has completed
+        gat.check_kube_pods(pytest.namespace, "manifest-indexing", "sowerjob")
+        # Get the indexd record and check if the hash value matches to the test_hash value
+        index_record = indexd.get_record(self.test_guid)
+        indexd_record_hash = index_record["hashes"]["md5"]
+        logger.debug(indexd_record_hash)
+        expect(indexd_record_hash).to.equal(self.test_hash)
 
-    def test_indexing_downloaded(self):
+    def test_indexing_download(self, page):
         """
         Scenario: Login and navigate to indexing page and download indexd manifest
         Steps:
@@ -28,17 +59,43 @@ class TestIndexingPage:
             2. Navigate to indexing page
             3. Click on Download and wait for manifest-indexing sower job to finish
             4. Send the request to the download url received from step 3
-            5. Verify the data
+            5. Verify the downloaded data
         """
-        pass
+        login_page = LoginPage()
+        indexing_page = IndexingPage()
+        # Go to login page and login with indexing_account user
+        login_page.go_to(page)
+        login_page.login(page, user="indexing_account")
+        # Go to indexing page and validate page is loaded
+        indexing_page.go_to(page)
+        manifest_link = indexing_page.download_manifest(page)
+        logger.debug(f"Download Link : {manifest_link}")
+        gat.check_kube_pods(pytest.namespace, "indexd-manifest", "sowerjob")
+        # sending request with manifest_link to get manifest data
+        manifest_link_resp = requests.get(manifest_link)
+        logger.debug(manifest_link_resp.text)
+        manifest_data = manifest_link_resp.text
+        assert (
+            self.expected_result in manifest_data
+        ), "Expected result not found in downloaded manifest"
 
-    def test_indexing_upload_invalid_manifest(self):
+    def test_indexing_upload_invalid_manifest(self, page):
         """
         Scenario:
         Steps:
             1. Login with indexing_account user
             2. Navigate to indexing page
             3. Upload invalid manifest and wait for manifest-indexing sower job to finish
-            4. Verify that that upload has failed
+            4. Verify that upload has failed
         """
-        pass
+        login_page = LoginPage()
+        indexing_page = IndexingPage()
+        # Go to login page and login with indexing_account user
+        login_page.go_to(page)
+        login_page.login(page, user="indexing_account")
+        # Go to indexing page and validate page is loaded
+        indexing_page.go_to(page)
+        # Upload the valid manifest via indexing page
+        indexing_page.upload_invalid_indexing_manifest(page)
+        # Check if the sowerjob pod for manifest-indexing has completed
+        gat.check_kube_pods(pytest.namespace, "manifest-indexing", "sowerjob", True)
