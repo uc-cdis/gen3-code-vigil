881c67eac328d38fc3011eddb3db39055d0fd0c0
Atharva Rane <atharvar@uchicago.edu>
2024-06-13 14:49:32 -0400

    address PR comments

--- gen3-integration-tests/tests/test_indexing_page.py
+++ gen3-integration-tests/tests/test_indexing_page.py
@@ -1,9 +1,11 @@
 import pytest
 import requests
-import os
 import utils.gen3_admin_tasks as gat
+import csv
+import os
 
-from utils import logger
+from uuid import uuid4
+from utils import logger, TEST_DATA_PATH_OBJECT
 from gen3.auth import Gen3Auth
 from gen3.index import Gen3Index
 from pages.login import LoginPage
@@ -18,15 +20,54 @@ class TestIndexingPage:
     variables = {}
 
     def setup_class(cls):
-        cls.variables["test_guid"] = "c2da639f-aa25-4c4d-8e89-02a143788268"
-        cls.variables["test_hash"] = (
-            "73d643ec3f4beb9020eef0beed440ad4"  # pragma: allowlist secret
-        )
+        cls.variables["test_guid"] = str(uuid4())
+        cls.variables["md5sum"] = "73d643ec3f4beb9020eef0beed440ad4"
         cls.variables["expected_result"] = (
             f"{cls.variables['test_guid']},"
             "s3://cdis-presigned-url-test/testdata,,jenkins2,"
-            f"{cls.variables['test_hash']},13,"
-        )  # pragma: allowlist secret
+            f"{cls.variables['md5sum']},13,"
+        )
+        # Updating the manifests with test_guid
+        valid_manifest_path = (
+            TEST_DATA_PATH_OBJECT / "indexing_page" / "valid_test_manifest.tsv"
+        )
+        invalid_manifest_path = (
+            TEST_DATA_PATH_OBJECT / "indexing_page" / "invalid_test_manifest.tsv"
+        )
+        cls.variables["valid_output_path"] = "valid_output.tsv"
+        cls.variables["invalid_output_path"] = "invalid_output.tsv"
+        # for valid_test_manifest
+        tsv_column = "GUID"
+        with open(valid_manifest_path, "r", newline="", encoding="utf-8") as input_file:
+            reader = csv.DictReader(input_file, delimiter="\t")
+            fieldnames = [tsv_column] + reader.fieldnames
+            with open(
+                cls.variables["valid_output_path"], "w", newline="", encoding="utf-8"
+            ) as output_file:
+                writer = csv.DictWriter(
+                    output_file, fieldnames=fieldnames, delimiter="\t"
+                )
+                writer.writeheader()
+                for row in reader:
+                    row[tsv_column] = cls.variables["test_guid"]
+                    writer.writerow(row)
+        # for invalid_test_manifest
+        invalid_tsv_column = "\ufeffGUID"
+        with open(
+            invalid_manifest_path, "r", newline="", encoding="utf-8"
+        ) as input_file:
+            reader = csv.DictReader(input_file, delimiter="\t")
+            fieldnames = [invalid_tsv_column] + reader.fieldnames
+            with open(
+                cls.variables["invalid_output_path"], "w", newline="", encoding="utf-8"
+            ) as output_file:
+                writer = csv.DictWriter(
+                    output_file, fieldnames=fieldnames, delimiter="\t"
+                )
+                writer.writeheader()
+                for row in reader:
+                    row[invalid_tsv_column] = str(uuid4())
+                    writer.writerow(row)
 
     def teardown_class(cls):
         # Delete the indexd record after the test
@@ -39,8 +80,20 @@ class TestIndexingPage:
             logger.exception(
                 msg=f"Failed to delete record for guid {cls.test_guid}: {e}"
             )
+        # Deleting output files after the test is done
+        for file_path in [
+            cls.variables["valid_output_path"],
+            cls.variables["invalid_output_path"],
+        ]:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+                logger.info(f"Deleted output file: {file_path}")
+            else:
+                logger.info(
+                    f"Output file '{file_path}' not found, so no deletion needed"
+                )
 
-    def test_indexing_upload_valid_manifest(self, page):
+    def test_indexing_upload_and_download_valid_manifest(self, page):
         """
         Scenario: Login and navigate to indexing page and upload dummy manifest
         Steps:
@@ -48,7 +101,10 @@ class TestIndexingPage:
             2. Navigate to indexing page
             3. Upload valid manifest and wait for manifest-indexing sower job to finish
             4. Check if the indexd record exists
-            5. Check if the indexd record hashes exists
+            5. Assert if the indexd record hashes matches
+            6. Navigate to indexing page and click on 'Download Manifest' button
+            7. Send the request to the download url received from step 3
+            8. Verify the downloaded data
         """
         login_page = LoginPage()
         indexing_page = IndexingPage()
@@ -59,41 +115,29 @@ class TestIndexingPage:
         # Go to indexing page and validate page is loaded
         indexing_page.go_to(page)
         # Upload the valid manifest via indexing page
-        indexing_page.upload_valid_indexing_manifest(page)
+        indexing_page.upload_valid_indexing_manifest(
+            page, self.variables["valid_output_path"]
+        )
         # Check if the sowerjob pod for manifest-indexing has completed
         gat.check_pod(pytest.namespace, "manifest-indexing", "sowerjob")
         # Get the indexd record and check if the hash value matches to the test_hash value
         index_record = indexd.get_record(self.variables["test_guid"])
         indexd_record_hash = index_record["hashes"]["md5"]
-        logger.info(indexd_record_hash)
+        logger.debug(indexd_record_hash)
         assert (
-            indexd_record_hash == self.variables["test_hash"]
-        ), f"Expected MD5 hash {self.variables['test_hash']}, but got {indexd_record_hash}"
-
-    def test_indexing_download(self, page):
-        """
-        Scenario: Login and navigate to indexing page and download indexd manifest
-        Steps:
-            1. Login with indexing_account user
-            2. Navigate to indexing page
-            3. Click on Download and wait for manifest-indexing sower job to finish
-            4. Send the request to the download url received from step 3
-            5. Verify the downloaded data
-        """
-        login_page = LoginPage()
-        indexing_page = IndexingPage()
-        # Go to login page and login with indexing_account user
-        login_page.go_to(page)
-        login_page.login(page, user="indexing_account")
-        # Go to indexing page and validate page is loaded
+            indexd_record_hash == self.variables["md5sum"]
+        ), f"Expected MD5 hash {self.variables['md5sum']}, but got {indexd_record_hash}"
+        # Go to indexing page after uploading the valid manifest to perform download manifest
         indexing_page.go_to(page)
-        manifest_link = indexing_page.download_manifest(page)
+        # Click on 'Download Manifest' button and wait to get the manifest link
+        manifest_link = indexing_page.get_manifest_download_link(page)
         logger.debug(f"Download Link : {manifest_link}")
         gat.check_pod(pytest.namespace, "indexd-manifest", "sowerjob")
-        # sending request with manifest_link to get manifest data
+        # Sending request with manifest_link to get manifest data
         manifest_link_resp = requests.get(manifest_link)
         logger.debug(manifest_link_resp.text)
         manifest_data = manifest_link_resp.text
+        # Verify downloaded data if it consists the uploaded manifest record
         assert (
             self.variables["expected_result"] in manifest_data
         ), "Expected result not found in downloaded manifest"
@@ -106,6 +150,11 @@ class TestIndexingPage:
             2. Navigate to indexing page
             3. Upload invalid manifest and wait for manifest-indexing sower job to finish
             4. Verify that upload has failed
+
+        Invalid Manifest - '\ufeff' at the beginning of your file content indicates a Byte-Order Mark,
+        is often used in text files to signal the encoding it can cause issues when parsing TSV.
+        Also, there is a newline sequence '\n' within the value for testGUID, and an extra comma
+        ',' at the end of the size value which can make the TSV content invalid or improperly formatted.
         """
         login_page = LoginPage()
         indexing_page = IndexingPage()
@@ -115,6 +164,8 @@ class TestIndexingPage:
         # Go to indexing page and validate page is loaded
         indexing_page.go_to(page)
         # Upload the valid manifest via indexing page
-        indexing_page.upload_invalid_indexing_manifest(page)
-        # Check if the sowerjob pod for manifest-indexing has completed
+        indexing_page.upload_invalid_indexing_manifest(
+            page, self.variables["invalid_output_path"]
+        )
+        # Check status of the sowerjob manifest-indexin pod , we expect the pods to fail as the manifest is invalid
         gat.check_pod(pytest.namespace, "manifest-indexing", "sowerjob", True)
