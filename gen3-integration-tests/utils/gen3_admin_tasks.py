0c012dce3e848de147222fb7c5df51f28c5a7b9f
Atharva Rane <atharvar@uchicago.edu>
2024-07-01 16:21:14 -0400

    deleting unnecessary jobs

--- gen3-integration-tests/utils/gen3_admin_tasks.py
+++ gen3-integration-tests/utils/gen3_admin_tasks.py
@@ -105,36 +105,6 @@ def run_gen3_job(test_env_namespace: str, job_name: str, roll_all: bool = False)
         raise Exception("Build number not found")
 
 
-def generate_test_data(
-    test_env_namespace: str,
-    max_examples: int,
-):
-    """
-    Runs jenkins job to generate test data
-    Since this requires adminvm interaction we use jenkins
-    """
-    job = JenkinsJob(
-        os.getenv("JENKINS_URL"),
-        os.getenv("JENKINS_USERNAME"),
-        os.getenv("JENKINS_PASSWORD"),
-        "generate-test-data",
-    )
-    params = {
-        "NAMESPACE": test_env_namespace,
-        "MAX_EXAMPLES": max_examples,
-    }
-    build_num = job.build_job(params)
-    if build_num:
-        status = job.wait_for_build_completion(build_num)
-        if status == "Completed":
-            return job.get_build_result(build_num)
-        else:
-            job.terminate_build(build_num)
-            raise Exception("Build timed out. Consider increasing max_duration")
-    else:
-        raise Exception("Build number not found")
-
-
 def check_job_pod(
     test_env_namespace: str,
     job_name: str,
@@ -145,7 +115,7 @@ def check_job_pod(
         os.getenv("JENKINS_URL"),
         os.getenv("JENKINS_USERNAME"),
         os.getenv("JENKINS_PASSWORD"),
-        "check-kube-pod",
+        "check-kube-job-pod",
     )
     params = {
         "NAMESPACE": test_env_namespace,
