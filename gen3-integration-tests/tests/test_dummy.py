9e00612da051fc83607a6e3e603555c1e0c1031b
Hara Prasad Juvvala <haraprasadj@uchicago.edu>
2024-07-10 11:01:47 -0700

    try removing gfm flavor

--- gen3-integration-tests/tests/test_dummy.py
+++ gen3-integration-tests/tests/test_dummy.py
@@ -5,9 +5,6 @@ class TestDummy:
     def test_dummy_pass(self):
         assert 1 == 1
 
-    def test_dummy_fail(self):
-        assert 1 == 2
-
     @pytest.mark.skip
     def test_dummy_skip(self):
         assert 1 == 1
