0541f89d3a3e5a91db0556a07d925b9d43c63e88
Atharva Rane <atharvar@uchicago.edu>
2024-07-22 17:15:00 -0400

    update passing test code

--- gen3-integration-tests/tests/test_dataguids.py
+++ gen3-integration-tests/tests/test_dataguids.py
@@ -2,17 +2,21 @@ import os
 import pytest
 import requests
 import json
+import time
+import re
 
 from utils import logger, TEST_DATA_PATH_OBJECT
-from playwright.sync_api import expect
-import utils.gen3_admin_tasks as gat
+from utils.test_execution import screenshot
+from playwright.sync_api import expect, Page
+from requests.exceptions import RequestException, ConnectionError, Timeout
 
 
 # TODO : enable this test after the manifest PRs are ready to roll
-@pytest.mark.wip
-class TestDataGuids:
+# @pytest.mark.wip
+class TestDataGuids(object):
     variables = {}
-    dataguids_url = "https://dataguids.org"
+    # dataguids_url = "https://dataguids.org"
+    dataguids_url = f"{pytest.root_url_portal}"
     non_existent_guids = [
         "dg.ABCD/0000b4b4-2af4-42e2-9bfa-6fd11e5fb97a",
         "0000b456-3r56-1dr3-0rt4-6fd11e5fb97a",
@@ -23,7 +27,9 @@ class TestDataGuids:
         cls.variables["guids"] = []
         logger.info("Getting all the hosts from manifest.json ...")
         try:
-            manifest_json = TEST_DATA_PATH_OBJECT / "configuration/manifest.json"
+            manifest_json = json.loads(
+                (TEST_DATA_PATH_OBJECT / "configuration/manifest.json").read_text()
+            )
         except FileNotFoundError:
             logger.error("manifest.json not found")
             raise
@@ -32,17 +38,28 @@ class TestDataGuids:
             nested_json = json.loads(nested_json_str)
             indexd_dist = nested_json.get("indexd", {}).get("dist", [])
             host_list = [entry.get("host") for entry in indexd_dist]
+            logger.debug(f"Host_List: {host_list}")
 
-        logger.info("Getting the first guid fom hosts ...")
-        for host in host_list:
-            record = requests.get(f"{host}index?limit=1")
-            record_data_json = record.json()
-            assert record_data_json, "Response is empty"
-            guid = record_data_json["did"]
-            cls.variables["guids"].append(guid)
-            logger.debug(f"GUID from {host}: {guid}")
+            logger.info("Getting the first guid fom hosts ...")
+            for host in host_list:
+                try:
+                    record = requests.get(f"{host}index?limit=1")
+                    record_data_json = record.json()
+                    logger.debug(f"Record_Data : {record_data_json}")
+                    if "records" in record_data_json and record_data_json["records"]:
+                        guid = record_data_json["records"][0]["did"]
+                        cls.variables["guids"].append(guid)
+                        logger.debug(f"GUID from {host}: {guid}")
+                    else:
+                        logger.error(
+                            f"No 'records' or empty 'records' key found in response from {host}"
+                        )
+                except requests.exceptions.RequestException as e:
+                    logger.error(f"Request to {host} failed : {e}")
+        else:
+            logger.error("No 'json' block found in manifest data")
 
-    def test_resolve_prefixes(self, page):
+    def test_resolve_prefixes(self, page: Page):
         """
         Scenario: Resolve GUIDS with different prefixes and without prefix
         Steps:
@@ -51,16 +68,25 @@ class TestDataGuids:
             3. And check if the result shows up
         """
         for correct_guid in self.variables["guids"]:
+            logger.info(f"Resolving GUID {correct_guid} ...")
             page.goto(self.dataguids_url)
-            page.get_by_role("#guidval").fill(correct_guid)
-            resolve_button = page.locator("#resolveit")
+            screenshot(page, "DataGUIDPageValidGUID")
+            page.locator("//input[@id='guidval']").fill(correct_guid)
+            resolve_button = page.locator("//button[@id='resolveit']")
             resolve_button.scroll_into_view_if_needed()
             resolve_button.click()
-            resolve_result = page.locator("#resolverresult")
+            time.sleep(5)
+            resolve_result = page.locator("//pre[@id='resolverresult']")
             resolve_result.wait_for()
-            expect(resolve_result).to_have_text(f'"id": "{correct_guid}"')
+            screenshot(page, "ResolvedGUID")
+            resolved_text = resolve_result.text_content()
+            resolved_json = json.loads(resolved_text)
+            resolved_id = resolved_json["data_object"]["id"]
+            assert (
+                resolved_id == correct_guid
+            ), f"Expected ID {correct_guid}, but got {resolved_id}"
 
-    def test_drs_endpoint_and_resolve_guid(self, page):
+    def test_drs_endpoint_and_resolve_guid(self, page: Page):
         """
         Scenario: Test if the DRS endpoint resolves GUID
         Steps:
@@ -68,11 +94,33 @@ class TestDataGuids:
             2. And check if the result shows up
         """
         for correct_guid in self.variables["guids"]:
-            page.goto(f"{self.dataguids_url}/ga4gh/dos/v1/dataobjects/{correct_guid}")
-            id_content = page.content()
-            expect(id_content).to_have_text(f'"id": "{correct_guid}"')
+            try:
+                page.goto(
+                    f"{self.dataguids_url}/ga4gh/dos/v1/dataobjects/{correct_guid}"
+                )
+                id_content = page.content()
+                logger.debug(f"Response content: {id_content}")
+                match = re.search(r"<pre>(.*?)</pre>", id_content, re.DOTALL)
+                if match:
+                    json_text = match.group(1).strip()  # Extracted JSON text
+                else:
+                    logger.error("Failed to find JSON in response")
+                    assert False, "JSON part not found in response"
+                try:
+                    json_data = json.loads(json_text)
+                    logger.debug(f"GA4GH request response : {json_data}")
+                except json.JSONDecodeError as e:
+                    logger.error(f"Failed to parse JSON: {e}")
+                    assert False, "Response is not valid JSON response"
+                guid = json_data.get("data_object", {}).get("id")
+                assert (
+                    guid == correct_guid
+                ), f"GUID {correct_guid} not found in response"
+            except (RequestException, ConnectionError, Timeout) as e:
+                logger.error(f"Request failed: {e}")
+                assert False, f"Request to {self.dataguids_url} failed: {e}"
 
-    def test_nonexistent_guid(self, page):
+    def test_nonexistent_guid(self, page: Page):
         """
         Scenario: Resolve GUIDS with nonexistent guids
         Steps:
@@ -81,16 +129,24 @@ class TestDataGuids:
             3. And check if the result shows up - 404 not found
         """
         for guid in self.non_existent_guids:
+            logger.info(f"Resolving GUID {guid} ...")
             page.goto(self.dataguids_url)
-            page.get_by_role("#guidval").fill(guid)
-            resolve_button = page.locator("#resolveit")
+            screenshot(page, "DataGUIDPageValidGUID")
+            page.locator("//input[@id='guidval']").fill(guid)
+            resolve_button = page.locator("//button[@id='resolveit']")
             resolve_button.scroll_into_view_if_needed()
             resolve_button.click()
-            resolve_result = page.locator("#resolverresult")
+            time.sleep(5)
+            resolve_result = page.locator("//pre[@id='resolverresult']")
             resolve_result.wait_for()
-            expect(resolve_result).to_have_text(f'Data GUID "{guid}" not found.')
+            screenshot(page, "ResolvedGUID")
+            resolved_text = resolve_result.text_content()
+            logger.debug(f"Resolved Text : {resolved_text}")
+            assert (
+                resolved_text == f'Data GUID "{guid}" not found.'
+            ), f'Expected "Data GUID \\"{guid}\\" not found." but got "{resolved_text}"'
 
-    def test_drs_nonexistent_guid(self, page):
+    def test_drs_nonexistent_guid(self, page: Page):
         """
         Scenario: Test if the DRS endpoint resolves GUID
         Steps:
@@ -100,4 +156,19 @@ class TestDataGuids:
         for guid in self.non_existent_guids:
             page.goto(f"{self.dataguids_url}/ga4gh/dos/v1/dataobjects/{guid}")
             id_content = page.content()
-            expect(id_content).to_have_text(f'Data GUID "{guid}" not found.')
+            logger.debug(f"Response content: {id_content}")
+            match = re.search(r"<pre>(.*?)</pre>", id_content, re.DOTALL)
+            if match:
+                json_text = match.group(1).strip()  # Extracted JSON text
+            else:
+                logger.error("Failed to find JSON in response")
+                assert False, "JSON part not found in response"
+            try:
+                json_data = json.loads(json_text)
+                logger.debug(f"JSON Data: {json_data}")
+            except json.JSONDecodeError as e:
+                logger.error(f"Failed to parse JSON: {e}")
+                assert False, "Response is not valid JSON response"
+            msg = json_data.get("msg")
+            logger.debug(f" no record found: {msg}")
+            assert msg == "no record found", f"GUID {guid} was found"
