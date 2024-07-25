b8faafe10d769be549ea2b61e92355adbb95a6f7
Atharva Rane <atharvar@uchicago.edu>
2024-07-25 11:32:13 -0400

    Migration - Study Registration Tests

--- gen3-integration-tests/pages/login.py
+++ gen3-integration-tests/pages/login.py
@@ -15,7 +15,7 @@ class LoginPage(object):
         # Locators
         self.READY_CUE = "//div[@class='nav-bar']"  # homepage navigation bar
         self.USERNAME_LOCATOR = "//div[@class='top-bar']//a[3]"  # username locator
-        self.POP_UP_BOX = "//div[@id='popup']"  # pop_up_box
+        self.POP_UP_BOX = "//div[@id='popup__box']"  # pop_up_box
         self.RAS_LOGIN_BUTTON = "//button[@type='submit']"
         self.RAS_USERNAME_INPUT = "//input[@id='USER']"
         self.RAS_PASSWORD_INPUT = "//input[@id='PASSWORD']"
@@ -24,6 +24,11 @@ class LoginPage(object):
         self.ORCID_USERNAME_INPUT = "//input[@id='username-input']"
         self.ORCID_PASSWORD_INPUT = "//input[@id='password']"
         self.ORCID_LOGIN_BUTTON = "//button[@id='signin-button']"
+        # user for heal env as heal as both Google login and dev login
+        # TODO : implement multiple login function below in login function
+        self.DEV_LOGIN_BUTTON = (
+            "//button[@class='login-page__entry-button g3-button g3-button--default']"
+        )
         self.LOGIN_BUTTON = "//button[contains(text(), 'Dev login') or contains(text(), 'Google') or contains(text(), 'BioData Catalyst Developer Login')]"
         self.USER_PROFILE_DROPDOWN = (
             "//i[@class='g3-icon g3-icon--user-circle top-icon-button__icon']"
@@ -44,6 +49,11 @@ class LoginPage(object):
         page.context.add_cookies(
             [{"name": "dev_login", "value": pytest.users[user], "url": self.BASE_URL}]
         )
+        # printing cookies if needed for debugging purposes
+        cookies = page.context.cookies()
+        for cookie in cookies:
+            logger.debug(f"{cookie['name']}={cookie['value']}")
+
         if idp == "ORCID":
             page.locator("//button[normalize-space()='ORCID Login']").click()
             self.orcid_login(page)
@@ -52,6 +62,7 @@ class LoginPage(object):
             self.ras_login(page)
         else:
             page.locator(self.LOGIN_BUTTON).click()
+            # self.multiple_login_button(page)
         screenshot(page, "AfterLogin")
         page.wait_for_selector(self.USERNAME_LOCATOR, state="attached")
 
@@ -68,6 +79,23 @@ class LoginPage(object):
         assert (
             access_token_cookie is not None
         ), "Access token cookie not found after login"
+
+    def multiple_login_button(self, page: Page):
+        """
+        Check if the commons has multiple login buttons from self.LOGIN_BUTTON
+        If it returns two or more buttons, it will select the first element and click on it
+        """
+        login_buttons = page.locator(self.LOGIN_BUTTON)
+        # check if there are multiple login buttons
+        count = login_buttons.count()
+        logger.info(f"Count of buttons {count}")
+        # if more than one login button, click the first one
+        if count > 1:
+            first_button = login_buttons.nth(0)
+            first_button.click()
+        else:
+            # else click the one that is available
+            login_buttons.click()
 
     def orcid_login(self, page: Page):
         # Perform ORCID Login
---
+++ gen3-integration-tests/pages/study_registration.py
@@ -0,0 +1,110 @@
+import pytest
+import os
+from utils import logger
+import time
+
+from utils.test_execution import screenshot
+from playwright.sync_api import Page, expect
+
+
+class StudyRegistrationPage(object):
+    def __init__(self):
+        # Endpoints
+        self.BASE_URL = f"{pytest.root_url_portal}/study-reg"
+        self.REQUEST_ACCESS_PATH = f"{self.BASE_URL}/request-access"
+
+        # Locators
+        self.SEARCH_BAR = "//input[@placeholder='Search studies by keyword...']"
+        self.STUDY_TABLE = "//div[@class='discovery-studies-container']"
+        self.STUDY_ROW = "(//tr[@class='ant-table-row ant-table-row-level-0 discovery-table__row'])[1]"
+        self.STUDY_CHECKBOX = ""
+        self.STUDY_DRAWER = "//div[@class='ant-drawer-body']"
+        self.REQUEST_ACCESS_REGISTER = (
+            "//button[@class='ant-btn ant-btn-text discovery-modal__request-button']"
+        )
+        self.REQUEST_ACCESS_FORM_PAGE = "//form[@id='generic-access-request-form']"
+        self.PROJECT_TITLE = (
+            "//textarea[@id='generic-access-request-form_Study Grant_doNotInclude']"
+        )
+        self.FIRST_NAME = "//input[@id='generic-access-request-form_First Name']"
+        self.LAST_NAME = "//input[@id='generic-access-request-form_Last Name']"
+        self.EMAIL = "//input[@id='generic-access-request-form_E-mail Address']"
+        self.INSTITUTE = (
+            "//input[@id='generic-access-request-form_Affiliated Institution']"
+        )
+        self.ROLE_RADIO_BUTTON = "//input[@value='Principal Investigator']"
+        self.REQUEST_SUBMIT_BUTTON = "//button[@type='submit']"
+        self.SUCCESS_MESSAGE = "//div[@class='ant-result ant-result-success']"
+        self.REGISTER_BUTTON = "//span[normalize-space()='Register This Study']"
+        self.REGISTER_STUDY_FORM_PAGE = "//form[@id='study-reg-form']"
+        self.STUDY_TITLE = "//span[@class='ant-select-selection-item']"
+        self.CEDAR_UUID_FIELD = (
+            "//input[@placeholder='Provide your CEDAR user UUID here']"
+        )
+        self.REGISTER_SUBMIT_BUTTON = "//span[normalize-space()='Submit']"
+
+    def search_study(self, page: Page, study_id: str):
+        page.wait_for_selector(self.SEARCH_BAR)
+        page.wait_for_selector(self.STUDY_TABLE)
+        screenshot(page, "LoadedStudyTable")
+        time.sleep(20)
+        page.locator(self.SEARCH_BAR).fill(study_id)
+        page.keyboard.press("Enter")
+        time.sleep(10)
+        screenshot(page, "SearchStudy")
+        first_study = page.locator(self.STUDY_ROW)
+        first_study.click()
+        screenshot(page, "ClickedCheckedBox")
+        page.wait_for_selector(self.STUDY_DRAWER)
+        screenshot(page, "StudyDrawer")
+
+    def click_request_access_to_register(self, page: Page):
+        register_request_access = page.locator(self.REQUEST_ACCESS_REGISTER)
+        register_request_access.wait_for(state="visible")
+        screenshot(page, "RegisterRequestAccessButton")
+        register_request_access.click()
+
+    def fill_request_access_form(self, page: Page, email: str, project_title: str):
+        current_url = page.url
+        assert (
+            current_url == self.REQUEST_ACCESS_PATH
+        ), f"Expected URL to be {self.REQUEST_ACCESS_PATH}, but got {current_url}"
+        # adding sleep here,as form page takes sometime to load after you click on request access button.
+        time.sleep(30)
+        screenshot(page, "RequestAccessForm")
+        access_form = page.locator(self.REQUEST_ACCESS_FORM_PAGE)
+        access_form.wait_for(state="visible")
+        project_title_field = page.locator(self.PROJECT_TITLE).input_value()
+        assert (
+            project_title_field == project_title
+        ), f"Expect Project Title to be {project_title}, but got {project_title_field}"
+        page.fill(self.FIRST_NAME, "Test")
+        page.fill(self.LAST_NAME, "User")
+        page.fill(self.EMAIL, email)
+        page.fill(self.INSTITUTE, "University of Chicago")
+        page.click(self.ROLE_RADIO_BUTTON)
+        screenshot(page, "FilledRequestRegistrationForm")
+        page.click(self.REQUEST_SUBMIT_BUTTON)
+        time.sleep(10)
+        screenshot(page, "SubmittedRequestAccessForm")
+
+    def click_register_study(self, page: Page):
+        register_button = page.locator(self.REGISTER_BUTTON)
+        register_button.wait_for(state="visible")
+        screenshot(page, "RegisterButton")
+        register_button.click()
+
+    def fill_registration_form(self, page: Page, uuid: str, study_name: str):
+        current_url = page.url
+        assert (
+            current_url == self.BASE_URL
+        ), f"Expected URL to be {self.BASE_URL}, but got {current_url}"
+        page.wait_for_selector(self.REGISTER_STUDY_FORM_PAGE, state="visible")
+        screenshot(page, "RegisterStudyForm")
+        study_title = page.locator(self.STUDY_TITLE)
+        expect(study_title).to_have_text(study_name)
+        page.fill(self.CEDAR_UUID_FIELD, uuid)
+        screenshot(page, "FilledRegistrationForm")
+        page.click(self.REGISTER_SUBMIT_BUTTON)
+        time.sleep(10)
+        screenshot(page, "AfterRegisterSubmitClick")
