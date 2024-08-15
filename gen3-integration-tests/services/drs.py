5665da8c514aee70f2495fb86d2540938c00d1eb
Krishna Agarwal <159047652+krishnaa05@users.noreply.github.com>
2024-08-15 13:53:57 -0500

    Migration - Fence DrsEndpoint Tests (#93)
    * drs_endpoint initial commit
    
    * changes for drs endpoint
    
    * updated docstring in drs.py
    
    * test_get_drs_invalid_access_id update
    
    * minor fixes
    
    * fix for sem_ver_version
    
    * Removing Semantic Version test case
    
    ---------
    
    Co-authored-by: Hara Prasad <haraprasadj@uchicago.edu>

---
+++ gen3-integration-tests/services/drs.py
@@ -0,0 +1,45 @@
+import pytest
+import requests
+
+from gen3.auth import Gen3Auth
+from uuid import uuid4
+from utils import logger
+
+
+class Drs(object):
+    def __init__(self):
+        self.BASE_URL = f"{pytest.root_url}"
+        self.DRS_ENDPOINT = "/ga4gh/drs/v1/objects"
+
+    def get_drs_object(self, file: dict, user="main_account"):
+        """Get Drs object"""
+        auth = Gen3Auth(refresh_token=pytest.api_keys[user], endpoint=self.BASE_URL)
+        try:
+            id = file.get("did") or file.get("id")
+        except Exception:
+            id = None
+        response = auth.curl(path=f"{self.DRS_ENDPOINT}/{id}")
+        return response
+
+    def get_drs_signed_url(self, file, user="main_account"):
+        """Get Drs signed url"""
+        auth = Gen3Auth(refresh_token=pytest.api_keys[user], endpoint=self.BASE_URL)
+        try:
+            id = file.get("did") or file.get("id")
+        except Exception:
+            id = None
+        access_id = file["urls"][0][:2]
+        response = auth.curl(path=f"{self.DRS_ENDPOINT}/{id}/access/{access_id}")
+        return response
+
+    def get_drs_signed_url_without_header(self, file, user="main_account"):
+        """Get Drs signed url without header"""
+        try:
+            id = file.get("did") or file.get("id")
+        except Exception:
+            id = None
+        access_id = file["urls"][0][:2]
+        response = requests.get(
+            url=f"{self.BASE_URL}{self.DRS_ENDPOINT}/{id}/access/{access_id}"
+        )
+        return response
