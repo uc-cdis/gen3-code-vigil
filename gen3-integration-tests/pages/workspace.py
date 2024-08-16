8ce8361b480392c3cb5034384c1c8a38185db902
Hara Prasad Juvvala <haraprasadj@uchicago.edu>
2024-08-16 07:17:56 -0700

    trying to terminate workspace left by the test

--- gen3-integration-tests/pages/workspace.py
+++ gen3-integration-tests/pages/workspace.py
@@ -55,7 +55,6 @@ class WorkspacePage(object):
         Launch the workspace with the given name
         Launches the first available one if `name` is not specified
         """
-        logger.info("Increasing capacity to speed up the launch")
         expect(page.locator(self.WORKSPACE_OPTIONS)).to_be_visible()
         # launch the specified workspace option if the name is provided
         logger.info(f"Launching workspace {name}")
