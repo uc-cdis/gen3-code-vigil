a08ebbbb0d477c116a4a878e29c9faea91704cd6
Krishna Agarwal <krishnaa@uchicago.edu>
2024-08-23 08:24:24 -0500

    Change for RAS Grant button

--- gen3-integration-tests/pages/login.py
+++ gen3-integration-tests/pages/login.py
@@ -140,7 +140,7 @@ class LoginPage(object):
         screenshot(page, "BeforeRASLogin")
         ras_login_button.click()
         # Handle the Grant access button
-        if page.locator(self.RAS_GRANT_BUTTON).is_enabled(timeout=5000):
+        if page.locator(self.RAS_GRANT_BUTTON).is_visible():
             logger.info("Clicking on Grant button")
             page.locator(self.RAS_GRANT_BUTTON).click()
         screenshot(page, "AfterRASLogin")
