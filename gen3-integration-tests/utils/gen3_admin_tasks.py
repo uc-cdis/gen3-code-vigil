6bbf252933a141fc0083e8e6da273db238cbc1ee
Krishna Agarwal <159047652+krishnaa05@users.noreply.github.com>
2024-08-30 09:11:47 -0500

    General fixes (#105)
    * General fix initial commit
    
    * GraphDataTools unwanted generation data removal

--- gen3-integration-tests/utils/gen3_admin_tasks.py
+++ gen3-integration-tests/utils/gen3_admin_tasks.py
@@ -2,7 +2,6 @@ import os
 import pytest
 import requests
 import time
-import json
 
 from dotenv import load_dotenv
 
@@ -122,7 +121,7 @@ def check_job_pod(
         os.getenv("JENKINS_URL"),
         os.getenv("JENKINS_USERNAME"),
         os.getenv("JENKINS_PASSWORD"),
-        "check-kube-job-pod",
+        "ci-only-check-kube-job-pod",
     )
     params = {
         "NAMESPACE": test_env_namespace,
@@ -153,7 +152,7 @@ def create_fence_client(
         os.getenv("JENKINS_URL"),
         os.getenv("JENKINS_USERNAME"),
         os.getenv("JENKINS_PASSWORD"),
-        "fence-create-client",
+        "ci-only-fence-create-client",
     )
     params = {
         "NAMESPACE": test_env_namespace,
@@ -215,7 +214,7 @@ def force_link_google(test_env_namespace: str, username: str, email: str):
         os.getenv("JENKINS_URL"),
         os.getenv("JENKINS_USERNAME"),
         os.getenv("JENKINS_PASSWORD"),
-        "fence-force-link-google",
+        "ci-only-fence-force-link-google",
     )
     params = {"NAMESPACE": test_env_namespace, "USERNAME": username, "EMAIL": email}
     build_num = job.build_job(params)
@@ -239,7 +238,7 @@ def delete_fence_client(test_env_namespace: str):
         os.getenv("JENKINS_URL"),
         os.getenv("JENKINS_USERNAME"),
         os.getenv("JENKINS_PASSWORD"),
-        "fence-delete-client",
+        "ci-only-fence-delete-client",
     )
     params = {
         "NAMESPACE": test_env_namespace,
@@ -265,7 +264,7 @@ def revoke_arborist_policy(test_env_namespace: str, username: str, policy: str):
         os.getenv("JENKINS_URL"),
         os.getenv("JENKINS_USERNAME"),
         os.getenv("JENKINS_PASSWORD"),
-        "revoke-arborist-policy",
+        "ci-only-revoke-arborist-policy",
     )
     params = {
         "NAMESPACE": test_env_namespace,
@@ -292,7 +291,7 @@ def update_audit_service_logging(test_env_namespace: str, audit_logging: str):
         os.getenv("JENKINS_URL"),
         os.getenv("JENKINS_USERNAME"),
         os.getenv("JENKINS_PASSWORD"),
-        "audit-service-logging",
+        "ci-only-audit-service-logging",
     )
     params = {
         "AUDIT_LOGGING": audit_logging,
@@ -318,7 +317,7 @@ def mutate_manifest_for_guppy_test(test_env_namespace: str):
         os.getenv("JENKINS_URL"),
         os.getenv("JENKINS_USERNAME"),
         os.getenv("JENKINS_PASSWORD"),
-        "mutate-manifest-for-guppy-test",
+        "ci-only-mutate-manifest-for-guppy-test",
     )
     params = {
         "NAMESPACE": test_env_namespace,
@@ -343,7 +342,7 @@ def clean_up_indices(test_env_namespace: str):
         os.getenv("JENKINS_URL"),
         os.getenv("JENKINS_USERNAME"),
         os.getenv("JENKINS_PASSWORD"),
-        "clean-up-indices",
+        "ci-only-clean-up-indices",
     )
     params = {"NAMESPACE": test_env_namespace}
     build_num = job.build_job(params)
@@ -366,7 +365,7 @@ def check_indices_after_etl(test_env_namespace: str):
         os.getenv("JENKINS_URL"),
         os.getenv("JENKINS_USERNAME"),
         os.getenv("JENKINS_PASSWORD"),
-        "check_indices_after_etl",
+        "ci-only-check_indices_after_etl",
     )
     params = {"NAMESPACE": test_env_namespace}
     build_num = job.build_job(params)
@@ -389,7 +388,7 @@ def create_access_token(test_env_namespace, service, expired, username):
         os.getenv("JENKINS_URL"),
         os.getenv("JENKINS_USERNAME"),
         os.getenv("JENKINS_PASSWORD"),
-        "create-access-token",
+        "ci-only-create-access-token",
     )
     if expired:
         expiration = 1
@@ -424,7 +423,7 @@ def kube_setup_service(test_env_namespace, servicename):
         os.getenv("JENKINS_URL"),
         os.getenv("JENKINS_USERNAME"),
         os.getenv("JENKINS_PASSWORD"),
-        "kube-setup-service",
+        "ci-only-kube-setup-service",
     )
     params = {
         "SERVICENAME": servicename,
@@ -475,7 +474,7 @@ def fence_enable_register_users_redirect(test_env_namespace: str):
         os.getenv("JENKINS_URL"),
         os.getenv("JENKINS_USERNAME"),
         os.getenv("JENKINS_PASSWORD"),
-        "fence-enable-user-register-redirect",
+        "ci-only-fence-enable-user-register-redirect",
     )
     params = {
         "NAMESPACE": test_env_namespace,
@@ -500,7 +499,7 @@ def fence_disable_register_users_redirect(test_env_namespace: str):
         os.getenv("JENKINS_URL"),
         os.getenv("JENKINS_USERNAME"),
         os.getenv("JENKINS_PASSWORD"),
-        "fence-disable-user-register-redirect",
+        "ci-only-fence-disable-user-register-redirect",
     )
     params = {
         "NAMESPACE": test_env_namespace,
