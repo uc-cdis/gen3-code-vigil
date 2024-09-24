58c3e57b8a9735deaa1afd4daf4773d2c2c3cad7
Krishna Agarwal <krishnaa@uchicago.edu>
2024-09-24 12:49:04 -0500

    change for OIDC client

--- gen3-integration-tests/tests/test_oidc_client.py
+++ gen3-integration-tests/tests/test_oidc_client.py
@@ -45,7 +45,9 @@ class TestOIDCClient:
             assert client_access_token, "Client access token was not created"
 
         # running fence-delete-expired-clients job
-        gat.run_gen3_job(pytest.namespace, "fence-delete-expired-clients")
+        gat.run_gen3_job(
+            test_env_namespace=pytest.namespace, job_name="fence-delete-expired-clients"
+        )
         # checking the logs from the job pod
         job_logs = gat.check_job_pod(
             pytest.namespace, "fence-delete-expired-clients", "gen3job"
