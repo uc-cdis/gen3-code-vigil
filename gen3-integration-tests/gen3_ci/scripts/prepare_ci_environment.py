eb88574b67ffabd202b4b00bf22d9af7acb199bb
Krishna Agarwal <krishnaa@uchicago.edu>
2024-11-15 10:11:21 -0600

    Add support for multiple QUAY_REPO image check

--- gen3-integration-tests/gen3_ci/scripts/prepare_ci_environment.py
+++ gen3-integration-tests/gen3_ci/scripts/prepare_ci_environment.py
@@ -22,18 +22,20 @@ def wait_for_quay_build(repo, tag):
     max_tries = 30  # Minutes to wait for image
     found = False
     i = 0
+    repo_list = repo.split(",")
     logger.info(f"Repo - {repo}, image - {tag}")
     while not found and i < max_tries:
-        logger.info(f"Waiting for image {repo}:{tag} to be built in quay")
-        res = requests.get(f"{quay_url_org}/{repo}/tag")
-        if res.status_code == 200:
-            branch_images = [x for x in res.json()["tags"] if x["name"] == tag]
-            if len(branch_images) >= 1:
-                image = branch_images[0]
-                image_time = datetime.utcfromtimestamp(image["start_ts"])
-                print(image_time)
-                if image_time > commit_time:
-                    found = True
+        for repo_item in repo_list:
+            logger.info(f"Waiting for image {repo_item}:{tag} to be built in quay")
+            res = requests.get(f"{quay_url_org}/{repo_item}/tag")
+            if res.status_code == 200:
+                branch_images = [x for x in res.json()["tags"] if x["name"] == tag]
+                if len(branch_images) >= 1:
+                    image = branch_images[0]
+                    image_time = datetime.utcfromtimestamp(image["start_ts"])
+                    print(image_time)
+                    if image_time > commit_time:
+                        found = True
         i += 1
         time.sleep(60)
     if found:
