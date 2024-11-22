41a0240f647f0283b72a1746540dbbc3fa82304c
Krishna Agarwal <krishnaa@uchicago.edu>
2024-11-22 08:41:34 -0600

    updated code

--- gen3-integration-tests/gen3_ci/jenkins_jobs/ci-only-modify-env-for-service-pr.groovy
+++ gen3-integration-tests/gen3_ci/jenkins_jobs/ci-only-modify-env-for-service-pr.groovy
@@ -123,11 +123,14 @@ spec:
             steps {
                 dir("cdis-manifest/${NAMESPACE}.planx-pla.net") {
                     script {
-                        currentBranch = "${SERVICE}:[a-zA-Z0-9._-]*"
-                        targetBranch = "${SERVICE}:${VERSION}"
-                        echo "Editing cdis-manifest/${NAMESPACE} service ${SERVICE} to version ${VERSION}"
-                        sh 'sed -i -e "s,'+"${currentBranch},${targetBranch}"+',g" manifest.json'
-                        sh 'cat manifest.json'
+                        serviceList = SERVICE.split(',')
+                        serviceList.each { SERVICE_NAME ->
+                          currentBranch = "${SERVICE_NAME}:[a-zA-Z0-9._-]*"
+                          targetBranch = "${SERVICE_NAME}:${VERSION}"
+                          echo "Editing cdis-manifest/${NAMESPACE} service ${SERVICE_NAME} to version ${VERSION}"
+                          sh 'sed -i -e "s,'+"${currentBranch},${targetBranch}"+',g" manifest.json'
+                          sh 'cat manifest.json'
+                        }
                     }
                 }
             }
