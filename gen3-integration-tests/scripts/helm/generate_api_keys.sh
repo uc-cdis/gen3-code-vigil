1bed7585cca365ab005e57a40402b108a6597973
J. Q. <55899496+jawadqur@users.noreply.github.com>
2025-02-17 10:36:36 -0600

    Update generate_api_keys.sh

--- gen3-integration-tests/scripts/helm/generate_api_keys.sh
+++ gen3-integration-tests/scripts/helm/generate_api_keys.sh
@@ -8,6 +8,7 @@ HOSTNAME=$2
 tail -n +2 "$USERS_FILE" | while IFS="," read -r username email; do
     echo "Processing user $username - $email ..."
     ACCESS_TOKEN=$(kubectl exec -c fence $(kubectl get pods -l app=fence -o jsonpath="{.items[0].metadata.name}") -- fence-create token-create --scopes openid,user,fence,data,credentials,google_service_account --type access_token --username $email 2>/dev/null | tail -1)
+    echo "ACCESS TOKEN: $ACCESS_TOKEN"
     curl -s -X POST "https://$HOSTNAME/user/credentials/api" -H "Authorization: bearer $ACCESS_TOKEN" -H "Content-Type: application/json" -H "Accept: aplication/json" > $HOME/.gen3/${HOSTNAME}_${username}.json 2>/dev/null
     echo "Saved API key in file ~/.gen3/${HOSTNAME}_${username}.json"
 done
