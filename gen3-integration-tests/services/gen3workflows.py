e1b7018ddb616c788110ca88a9d8a26125197771
Sai Shanmukha <nss10@outlook.com>
2025-02-17 14:24:54 -0600

    Responding to PR comments on `/s3` endpoint

--- gen3-integration-tests/services/gen3workflows.py
+++ gen3-integration-tests/services/gen3workflows.py
@@ -1,11 +1,51 @@
+from dataclasses import dataclass
 from typing import Dict, Tuple
 
 import boto3
 import botocore.exceptions
 import pytest
 import requests
+from botocore.config import Config
 from gen3.auth import Gen3Auth
 from utils import logger
+
+
+@dataclass(frozen=True)
+class WorkflowStorageConfig:
+    """
+    Represents the storage configuration details required for working with the S3 bucket in Gen3Workflow.
+
+    Attributes:
+        bucket (str): The name of the S3 bucket.
+        workdir (str): The full S3 URI for the working directory within the bucket.
+        region (str): The AWS region where the S3 bucket is located.
+    """
+
+    bucket_name: str
+    working_directory: str
+    bucket_region: str
+
+    @classmethod
+    def from_dict(cls, data: dict) -> "WorkflowStorageConfig":
+        """
+        Creates an instance of StorageConfig from a dictionary.
+
+        Args:
+            data (dict): A dictionary containing storage configuration details.
+
+        Returns:
+            StorageConfig: An instance of the StorageConfig class.
+        """
+        required_keys = ["bucket", "workdir", "region"]
+
+        # Ensure all required keys are present
+        for key in required_keys:
+            if key not in data:
+                raise ValueError(f"Missing value for key: '{key}' in StorageConfig")
+
+        return cls(
+            bucket=data["bucket"], workdir=data["workdir"], region=data["region"]
+        )
 
 
 class Gen3Workflow:
@@ -19,13 +59,16 @@ class Gen3Workflow:
         auth = Gen3Auth(refresh_token=pytest.api_keys[user], endpoint=self.BASE_URL)
         return auth.get_access_token()
 
-    def _get_s3_client(self, access_token: str):
+    def _get_s3_client(
+        self, access_token: str, s3_storage_config: WorkflowStorageConfig = "us-east-1"
+    ):
         """Creates and returns an S3 client."""
         return boto3.client(
             service_name="s3",
             aws_access_key_id=access_token,
-            aws_secret_access_key="N/A",  # pragma: allowlist secret
+            aws_secret_access_key="N/A",
             endpoint_url=self.S3_ENDPOINT_URL,
+            config=Config(region_name=s3_storage_config.bucket_region),
         )
 
     def _get_bucket_and_key(self, object_path: str) -> Tuple[str, str]:
@@ -36,19 +79,23 @@ class Gen3Workflow:
 
     def get_storage_info(self, user: str = "main_account", expected_status=200) -> Dict:
         """Makes a GET request to the `/storage/info` endpoint."""
-        access_token = self._get_access_token(user) if user else None
         storage_url = f"{self.BASE_URL}{self.SERVICE_URL}/storage/info"
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"bearer {access_token}",
-        }
+        headers = (
+            {
+                "Content-Type": "application/json",
+                "Authorization": f"bearer {self._get_access_token(user)}",
+            }
+            if user
+            else {}
+        )
 
         response = requests.get(url=storage_url, headers=headers)
         assert (
             response.status_code == expected_status
-        ), f"Expected {expected_status}, got {response.status_code}"
-
-        return response.json()
+        ), f"Expected {expected_status}, got {response.status_code} when making a GET request to {storage_url}"
+        storage_info = response.json()
+        assert isinstance(storage_info, dict), "Expected a valid JSON response"
+        return storage_info
 
     def get_bucket_object_with_unsigned_request(
         self, object_path: str, user: str = "main_account", expected_status=401
@@ -57,23 +104,23 @@ class Gen3Workflow:
         access_token = self._get_access_token(user)
         s3_url = f"{self.S3_ENDPOINT_URL}/{object_path}"
         headers = {"Authorization": f"bearer {access_token}"}
-
         response = requests.get(url=s3_url, headers=headers)
         assert (
             response.status_code == expected_status
-        ), f"Expected {expected_status}, got {response.status_code}"
+        ), f"Expected {expected_status}, got {response.status_code} when attempting to make an unsigned GET request to {s3_url}"
 
-    def perform_s3_action(
+    def _perform_s3_action(
         self,
         action: str,
         object_path: str,
+        s3_storage_config: WorkflowStorageConfig,
         user: str = "main_account",
         expected_status=200,
         content: str = None,
     ):
         """Generic function for performing S3 actions like GET, PUT, DELETE."""
         access_token = self._get_access_token(user)
-        client = self._get_s3_client(access_token)
+        client = self._get_s3_client(access_token, s3_storage_config)
         bucket, key = self._get_bucket_and_key(object_path)
 
         if action == "list":
@@ -90,15 +137,21 @@ class Gen3Workflow:
         assert (
             response.get("ResponseMetadata", {}).get("HTTPStatusCode")
             == expected_status
-        ), f"Expected {expected_status}, got {response['ResponseMetadata']['HTTPStatusCode']}"
+        ), f"Expected {expected_status}, got {response['ResponseMetadata']['HTTPStatusCode']} when making an s3 request to perform {action} action on {bucket=} and {key=} "
         return response
 
     def get_bucket_object_with_boto3(
-        self, object_path: str, user: str = "main_account", expected_status=200
+        self,
+        object_path: str,
+        s3_storage_config: WorkflowStorageConfig,
+        user: str = "main_account",
+        expected_status=200,
     ):
         """Retrieves an S3 object."""
         try:
-            response = self.perform_s3_action("get", object_path, user, expected_status)
+            response = self._perform_s3_action(
+                "get", object_path, s3_storage_config, user, expected_status
+            )
             return response["Body"].read().decode("utf-8")
         except botocore.exceptions.ClientError as e:
             error_code = e.response.get("Error", {}).get("Code", "")
@@ -110,24 +163,39 @@ class Gen3Workflow:
             raise  # Reraise for other errors
 
     def list_bucket_objects_with_boto3(
-        self, folder_path: str, user: str = "main_account", expected_status=200
+        self,
+        folder_path: str,
+        s3_storage_config: WorkflowStorageConfig,
+        user: str = "main_account",
+        expected_status=200,
     ):
         """Retrieves a list of all S3 objects present in the given folder path."""
-        response = self.perform_s3_action("list", folder_path, user, expected_status)
+        response = self._perform_s3_action(
+            "list", folder_path, s3_storage_config, user, expected_status
+        )
         return response.get("Contents")
 
     def put_bucket_object_with_boto3(
         self,
         content: str,
         object_path: str,
+        s3_storage_config: WorkflowStorageConfig,
         user: str = "main_account",
         expected_status=200,
     ):
         """Uploads an object to S3."""
-        self.perform_s3_action("put", object_path, user, expected_status, content)
+        self._perform_s3_action(
+            "put", object_path, s3_storage_config, user, expected_status, content
+        )
 
     def delete_bucket_object_with_boto3(
-        self, object_path: str, user: str = "main_account", expected_status=200
+        self,
+        object_path: str,
+        s3_storage_config: WorkflowStorageConfig,
+        user: str = "main_account",
+        expected_status=200,
     ):
         """Deletes an object from S3."""
-        self.perform_s3_action("delete", object_path, user, expected_status)
+        self._perform_s3_action(
+            "delete", object_path, s3_storage_config, user, expected_status
+        )
--- gen3-integration-tests/tests/test_gen3_workflows.py
+++ gen3-integration-tests/tests/test_gen3_workflows.py
@@ -1,7 +1,7 @@
 import json
 
 import pytest
-from services.gen3workflows import Gen3Workflow
+from services.gen3workflows import Gen3Workflow, WorkflowStorageConfig
 from utils import TEST_DATA_PATH_OBJECT
 
 
@@ -24,9 +24,13 @@ class TestGen3Workflow(object):
     gen3_workflow = Gen3Workflow()
     valid_user = "main_account"
     invalid_user = "dummy_one"
-    bucket_name = None
-    folder_name = "integration-tests"
-    file_name = "test-input.txt"
+    s3_bucket_name = None
+    s3_folder_name = "integration-tests"
+    s3_file_name = "test-input.txt"
+    storage_info_dict = gen3_workflow.get_storage_info(
+        user=valid_user, expected_status=200
+    )
+    s3_storage_config = WorkflowStorageConfig.from_dict(storage_info_dict)
 
     ######################## Test /storage/info endpoint ########################
     def test_get_storage_info_with_valid_token(self):
@@ -34,11 +38,7 @@ class TestGen3Workflow(object):
         response = self.gen3_workflow.get_storage_info(
             user=self.valid_user, expected_status=200
         )
-        assert isinstance(response, dict), "Expected a valid JSON response"
-        assert (
-            "bucket" in response
-        ), "Expected the response to contain the `S3 bucket` details"
-        self.bucket_name = response["bucket"]
+        self.s3_bucket_name = response["bucket"]
 
     def test_get_storage_info_without_token(self):
         """Test GET /storage/info without an access token."""
@@ -47,44 +47,46 @@ class TestGen3Workflow(object):
     ######################## Test /s3/ endpoint ########################
 
     @pytest.mark.skipif(
-        not bucket_name,
+        not s3_bucket_name,
         reason="Skipping this test, since a valid s3 bucket name couldn't be found ",
     )
     def test_any_user_cannot_get_s3_file_with_unsigned_request(self):
         """Unsigned requests should receive 401 even if the user is authorized to get data."""
         self.gen3_workflow.get_bucket_object_with_unsigned_request(
-            object_path=f"{self.bucket_name}/{self.folder_name}/{self.file_name}",
+            object_path=f"{self.s3_bucket_name}/{self.s3_folder_name}/{self.s3_file_name}",
             user=self.valid_user,
             expected_status=401,
         )
 
     @pytest.mark.skipif(
-        not bucket_name,
+        not s3_bucket_name,
         reason="Skipping this test, since a valid s3 bucket name couldn't be found ",
     )
     def test_unauthorized_user_cannot_post_s3_file(self):
         """Unauthorized user should receive 403 when posting a S3 file."""
         self.gen3_workflow.put_bucket_object_with_boto3(
             content="dummy_S3_content",
-            object_path=f"{self.bucket_name}/{self.folder_name}/{self.file_name}",
+            object_path=f"{self.s3_bucket_name}/{self.s3_folder_name}/{self.s3_file_name}",
+            s3_storage_config=self.s3_storage_config,
             user=self.invalid_user,
             expected_status=403,
         )
 
     @pytest.mark.skipif(
-        not bucket_name,
+        not s3_bucket_name,
         reason="Skipping this test, since a valid s3 bucket name couldn't be found ",
     )
     def test_unauthorized_user_cannot_get_s3_file(self):
         """Unauthorized user should receive 403 when trying to GET a S3 file."""
         self.gen3_workflow.get_bucket_object_with_boto3(
-            object_path=f"{self.bucket_name}/{self.folder_name}/{self.file_name}",
+            object_path=f"{self.s3_bucket_name}/{self.s3_folder_name}/{self.s3_file_name}",
+            s3_storage_config=self.s3_storage_config,
             user=self.invalid_user,
             expected_status=403,
         )
 
     @pytest.mark.skipif(
-        not bucket_name,
+        not s3_bucket_name,
         reason="Skipping this test, since a valid s3 bucket name couldn't be found ",
     )
     def test_happy_path_upload_and_get_s3_file(self):
@@ -97,28 +99,31 @@ class TestGen3Workflow(object):
         input_content = "sample_S3_content"
         self.gen3_workflow.put_bucket_object_with_boto3(
             content=input_content,
-            object_path=f"{self.bucket_name}/{self.folder_name}/{self.file_name}",
+            object_path=f"{self.s3_bucket_name}/{self.s3_folder_name}/{self.s3_file_name}",
+            s3_storage_config=self.s3_storage_config,
             user=self.valid_user,
             expected_status=200,
         )
 
         # Fetch all the contents in the given folder to verify the list-objects functionality
         response_contents = self.gen3_workflow.list_bucket_objects_with_boto3(
-            folder_path=f"{self.bucket_name}/{self.folder_name}/",
+            folder_path=f"{self.s3_bucket_name}/{self.s3_folder_name}/",
+            s3_storage_config=self.s3_storage_config,
             user=self.valid_user,
             expected_status=200,
         )
         assert (
             isinstance(response_contents, list) and len(response_contents) > 0
-        ), "Expected the function to return a list of at least one item"
+        ), "Expected the function to return a list of at least one item but received: {response_contents}"
         assert (
             "Key" in response_contents[0]
-            and response_contents["Key"] == f"{self.folder_name}/{self.file_name}"
-        ), "Expected folder integration-tests in the bucket to have file `test-input.txt`"
+            and response_contents["Key"] == f"{self.s3_folder_name}/{self.s3_file_name}"
+        ), f"Expected folder {self.s3_folder_name} in the bucket to have file `{self.s3_file_name}` but received {response_contents}"
 
         # Fetch the exact file to verify the get-object functionality
         response_s3_object = self.gen3_workflow.get_bucket_object_with_boto3(
-            object_path=f"{self.bucket_name}/{self.folder_name}/{self.file_name}",
+            object_path=f"{self.s3_bucket_name}/{self.s3_folder_name}/{self.s3_file_name}",
+            s3_storage_config=self.s3_storage_config,
             user=self.valid_user,
             expected_status=200,
         )
@@ -128,20 +133,35 @@ class TestGen3Workflow(object):
         ), "Stored and retrieved content should match"
 
     @pytest.mark.skipif(
-        not bucket_name,
+        not s3_bucket_name,
         reason="Skipping this test, since a valid s3 bucket name couldn't be found ",
     )
     def test_happy_path_delete_s3_file(self):
-        """DELETE a S3 file, then attempt GET to verify it's gone."""
+        """
+        POST an S3 file in a `directoy/filename` format,
+        then, DELETE the S3 file,
+        then attempt GET to verify it's gone.
+        """
+        input_content = "sample_S3_content"
+        self.gen3_workflow.put_bucket_object_with_boto3(
+            content=input_content,
+            object_path=f"{self.s3_bucket_name}/{self.s3_folder_name}/{self.s3_file_name}",
+            s3_storage_config=self.s3_storage_config,
+            user=self.valid_user,
+            expected_status=200,
+        )
+
         self.gen3_workflow.delete_bucket_object_with_boto3(
-            object_path=f"{self.bucket_name}/{self.folder_name}/{self.file_name}",
+            object_path=f"{self.s3_bucket_name}/{self.s3_folder_name}/{self.s3_file_name}",
+            s3_storage_config=self.s3_storage_config,
             user=self.valid_user,
             expected_status=200,
         )
 
         # We expect the request to return a response with status 404
         self.gen3_workflow.get_bucket_object_with_boto3(
-            object_path=f"{self.bucket_name}/{self.folder_name}/{self.file_name}",
+            object_path=f"{self.s3_bucket_name}/{self.s3_folder_name}/{self.s3_file_name}",
+            s3_storage_config=self.s3_storage_config,
             user=self.valid_user,
             expected_status=404,
         )
