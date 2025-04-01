90093f7f5100334256d454ac351027ea77216e9b
Krishna Agarwal <krishnaa@uchicago.edu>
2025-04-01 14:10:14 -0500

    Add few updates for manifest PR section

--- gen3-integration-tests/test_data/test_setup/gen3_helm_env_setup/env_setup.sh
+++ gen3-integration-tests/test_data/test_setup/gen3_helm_env_setup/env_setup.sh
@@ -10,27 +10,86 @@
 namespace="$1"
 setup_type="$2"
 
-# If PR is under test repository, then do nothing
 if [ "$setup_type" == "test-env-setup" ] ; then
+    # If PR is under test repository, then do nothing
     echo "Setting Up Test PR Env..."
-# If PR is under a service repository, then update the image for the given service
 elif [ "$setup_type" == "service-env-setup" ]; then
+    # If PR is under a service repository, then update the image for the given service
+    echo "Setting Up Service PR Env..."
     # Inputs:
     # service_name - name of service against which PR is run
     # image_name - name of the quay image for the service PR
-    echo "Setting Up Service PR Env..."
     service_name="$3"
     image_name="$4"
-    echo "yq eval ".${service_name}.image.tag = \"${image_name}\"" -i values.yaml"
     yq eval ".${service_name}.image.tag = \"${image_name}\"" -i values.yaml
-# If PR is under a manifest repository, then update the yaml files as needed
 elif [ "$setup_type" == "manifest-env-setup" ]; then
+    # If PR is under a manifest repository, then update the yaml files as needed
     echo "Setting Up Manifest PR Env..."
+    # Inputs:
+    # cdis_manifest - name of the folder containing default ci env configuration
+    # temp_cdis_manifest - name of the folder containing manifest env configuration
+    cdis_manifest="${3}/unfunded/gen3.datacommons.io/values"
+    temp_cdis_manifest="${4}/unfunded/diseasedatahub.org/values"
+    echo "CI Default Env Configuration Folder Path: ${cdis_manifest}"
+    echo "New Env Configuration Folder Path: ${temp_cdis_manifest}"
+
+    ###########################################################################
+    # Make sure the blocks of values.yaml in cdis_manifest are in reflection of
+    # the blocks of values.yaml in temp_cdis_manifest.
+    ###########################################################################
+    # Get all the top-level keys from both files
+    keys_file1=$(yq eval 'keys' $cdis_manifest/values.yaml -o=json | jq -r '.[]')
+    keys_file2=$(yq eval 'keys' $temp_cdis_manifest/values.yaml -o=json | jq -r '.[]')
+
+    # Remove blocks from file1 that are not present in file2
+    for key in $keys_file1; do
+    if ! echo "$keys_file2" | grep -q "^$key$"; then
+        yq eval "del(.$key)" -i $cdis_manifest/values.yaml
+    fi
+    done
+
+    # Add blocks from file2 that are not present in file1
+    for key in $keys_file2; do
+    if ! echo "$keys_file1" | grep -q "^$key$"; then
+        yq eval ". |= . + {\"$key\": $(yq eval .$key $temp_cdis_manifest/values.yaml -o=json)}" -i $cdis_manifest/values.yaml
+    fi
+    done
+
+    ###########################################################################
+    # Update image.tags for each block of values.yaml in cdis_manifest
+    # with image.tags for each block of values.yaml in $temp_cdis_manifest.
+    ###########################################################################
+    for key in $keys_file2; do
+    image_tag_value=$(yq eval ".${key}.image.tag" "$file2" 2>/dev/null)
+    if [ ! -z "$image_tag_value" ]; then
+        yq eval ".${key}.image.tag = \"$image_tag_value\"" -i "$file1"
+    fi
+    done
+
+    # Perform operations for global and other sections under values.yaml
+    keys=("global.dictionaryUrl"
+     "global.portalApp"
+     "global.netpolicy"
+     "global.frontendRoot"
+     "global.es7"
+     "google.enabled"
+     "ssjdispatcher.indexing"
+     )
+    for key in "${keys[@]}"; do
+        ci_value=$(yq eval ".$key // \"key not found\"" $cdis_manifest/values.yaml)
+        manifest_value=$(yq eval ".$key // \"key not found\"" $temp_cdis_manifest/values.yaml)
+        if [ "$manifest_value" = "key not found" ]; then
+            echo "The key '$key' is not present in the YAML file."
+        else
+            echo "CI default value of the key '$key' is: $ci_value"
+            echo "Manifest value of the key '$key' is: $manifest_value"
+        fi
+    done
 fi
 
 # Perform general steps to bring up the env after the values are set.
-helm upgrade --install gen3 gen3/gen3 -f values.yaml
-sleep 60
-kubectl wait --for=condition=Ready pod -l app=gen3-elasticsearch-master --namespace=${namespace} --timeout=5m
-kubectl create job --from=cronjob/usersync usersync-manual -n ${namespace}
-kubectl wait --for=condition=complete job/usersync-manual --namespace=${namespace} --timeout=5m
+# helm upgrade --install gen3 gen3/gen3 -f values.yaml
+# sleep 60
+# kubectl wait --for=condition=Ready pod -l app=gen3-elasticsearch-master --namespace=${namespace} --timeout=5m
+# kubectl create job --from=cronjob/usersync usersync-manual -n ${namespace}
+# kubectl wait --for=condition=complete job/usersync-manual --namespace=${namespace} --timeout=5m
