47d221e5014447debfb2e1825de492011f00eb5e
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2025-04-15 10:21:25 -0500

    DICOM tests: Take screenshot earlier (#177)

--- gen3-integration-tests/pages/dicom.py
+++ gen3-integration-tests/pages/dicom.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 from playwright.sync_api import Page, expect
 from utils import logger
@@ -15,18 +17,21 @@ class DicomPage(object):
 
     def goto_explorer_page(self, page: Page, study_id):
         page.goto(self.EXPLORER_ENDPOINT)
+        time.sleep(2)  # wait for page to load
         screenshot(page, "ExplorerPage")
         imaging_studies_tab = page.locator(self.IMAGING_STUDIES_TAB)
         expect(imaging_studies_tab).to_be_visible(timeout=5000)
         imaging_studies_tab.click()
+        time.sleep(2)  # wait for page to load
         screenshot(page, "ImagingStudiesPage")
         STUDY_ID_HREF_XPATH = f"//a[contains(@href, 'StudyInstanceUIDs={study_id}')][1]"
         study_id_href = page.locator(STUDY_ID_HREF_XPATH)
         expect(study_id_href).to_be_visible(timeout=30000)
         href_url = study_id_href.get_attribute("href")
         page.goto(href_url)
-        logger.info(page.url)
+        logger.info(f"Current URL: {page.url}")
         assert study_id in page.url, f"Expected {study_id} in {page.url}"
         cornerstone_canvas = page.locator(self.CORNERSTONE_CANVAS)
-        expect(cornerstone_canvas).to_be_visible(timeout=30000)
+        time.sleep(2)  # wait for page to load
         screenshot(page, "OHIFViewerPage")
+        expect(cornerstone_canvas).to_be_visible(timeout=30000)
