3d7c8c946e899501ca29d6502e26413d4cfead4d
EliseCastle23 <109446148+EliseCastle23@users.noreply.github.com>
2025-04-28 09:36:00 -0600

    uncommenting gitops setup

--- gen3-integration-tests/scripts/helm/env_setup.sh
+++ gen3-integration-tests/scripts/helm/env_setup.sh
@@ -24,104 +24,104 @@ elif [ "$setup_type" == "service-env-setup" ]; then
     service_name="$4"
     image_name="$5"
     yq eval ".${service_name}.image.tag = \"${image_name}\"" -i values.yaml
-# elif [ "$setup_type" == "manifest-env-setup" ]; then
-#     # If PR is under a manifest repository, then update the yaml files as needed
-#     echo "Setting Up Manifest PR Env..."
-#     # Inputs:
-#     # cdis_manifest - name of the folder containing default ci env configuration
-#     # temp_cdis_manifest - name of the folder containing manifest env configuration
-#     cdis_manifest="${4}/unfunded/gen3.datacommons.io/values"
-#     temp_cdis_manifest="${5}/unfunded/diseasedatahub.org/values"
-#     echo "CI Default Env Configuration Folder Path: ${cdis_manifest}"
-#     echo "New Env Configuration Folder Path: ${temp_cdis_manifest}"
+elif [ "$setup_type" == "manifest-env-setup" ]; then
+    # If PR is under a manifest repository, then update the yaml files as needed
+    echo "Setting Up Manifest PR Env..."
+    # Inputs:
+    # cdis_manifest - name of the folder containing default ci env configuration
+    # temp_cdis_manifest - name of the folder containing manifest env configuration
+    cdis_manifest="${4}/unfunded/gen3.datacommons.io/values"
+    temp_cdis_manifest="${5}/unfunded/diseasedatahub.org/values"
+    echo "CI Default Env Configuration Folder Path: ${cdis_manifest}"
+    echo "New Env Configuration Folder Path: ${temp_cdis_manifest}"
 
-#     ###############################################################################
-#     # Make sure the blocks of values.yaml in cdis_manifest are in reflection of
-#     # the blocks of values.yaml in temp_cdis_manifest.
-#     ###############################################################################
-#     # Get all the top-level keys from both files
-#     keys_ci=$(yq eval 'keys' $cdis_manifest/values.yaml -o=json | jq -r '.[]')
-#     keys_manifest=$(yq eval 'keys' $temp_cdis_manifest/values.yaml -o=json | jq -r '.[]')
+    ###############################################################################
+    # Make sure the blocks of values.yaml in cdis_manifest are in reflection of
+    # the blocks of values.yaml in temp_cdis_manifest.
+    ###############################################################################
+    # Get all the top-level keys from both files
+    keys_ci=$(yq eval 'keys' $cdis_manifest/values.yaml -o=json | jq -r '.[]')
+    keys_manifest=$(yq eval 'keys' $temp_cdis_manifest/values.yaml -o=json | jq -r '.[]')
 
-#     # Remove blocks from $cdis_manifest/values.yaml that are not present in $temp_cdis_manifest/values.yaml
-#     for key in $keys_ci; do
-#     if ! echo "$keys_manifest" | grep -q "^$key$"; then
-#         yq eval "del(.$key)" -i $cdis_manifest/values.yaml
-#     fi
-#     done
+    # Remove blocks from $cdis_manifest/values.yaml that are not present in $temp_cdis_manifest/values.yaml
+    for key in $keys_ci; do
+    if ! echo "$keys_manifest" | grep -q "^$key$"; then
+        yq eval "del(.$key)" -i $cdis_manifest/values.yaml
+    fi
+    done
 
-#     # Add blocks from $temp_cdis_manifest/values.yaml that are not present in $cdis_manifest/values.yaml
-#     for key in $keys_manifest; do
-#     if ! echo "$keys_ci" | grep -q "^$key$"; then
-#         yq eval ". |= . + {\"$key\": $(yq eval .$key $temp_cdis_manifest/values.yaml -o=json)}" -i $cdis_manifest/values.yaml
-#     fi
-#     done
+    # Add blocks from $temp_cdis_manifest/values.yaml that are not present in $cdis_manifest/values.yaml
+    for key in $keys_manifest; do
+    if ! echo "$keys_ci" | grep -q "^$key$"; then
+        yq eval ". |= . + {\"$key\": $(yq eval .$key $temp_cdis_manifest/values.yaml -o=json)}" -i $cdis_manifest/values.yaml
+    fi
+    done
 
-#     ###############################################################################
-#     # Update images for each service from $temp_cdis_manifest/values.yaml
-#     ###############################################################################
-#     for key in $keys_manifest; do
-#     image_tag_value=$(yq eval ".${key}.image.tag" $temp_cdis_manifest/values.yaml 2>/dev/null)
-#     if [ ! -z "$image_tag_value" ]; then
-#         yq eval ".${key}.image.tag = \"$image_tag_value\"" -i $cdis_manifest/values.yaml
-#     fi
-#     done
+    ###############################################################################
+    # Update images for each service from $temp_cdis_manifest/values.yaml
+    ###############################################################################
+    for key in $keys_manifest; do
+    image_tag_value=$(yq eval ".${key}.image.tag" $temp_cdis_manifest/values.yaml 2>/dev/null)
+    if [ ! -z "$image_tag_value" ]; then
+        yq eval ".${key}.image.tag = \"$image_tag_value\"" -i $cdis_manifest/values.yaml
+    fi
+    done
 
-#     ###############################################################################
-#     # Perform operations for global and other sections under values.yaml
-#     ###############################################################################
-#     keys=("global.dictionaryUrl"
-#      "global.portalApp"
-#      "global.netpolicy"
-#      "global.frontendRoot"
-#      "global.es7"
-#      "google.enabled"
-#      "ssjdispatcher.indexing"
-#      )
-#     for key in "${keys[@]}"; do
-#         ci_value=$(yq eval ".$key // \"key not found\"" $cdis_manifest/values.yaml)
-#         manifest_value=$(yq eval ".$key // \"key not found\"" $temp_cdis_manifest/values.yaml)
-#         if [ "$manifest_value" = "key not found" ]; then
-#             echo "The key '$key' is not present in the Manifest YAML file."
-#         else
-#             echo "CI default value of the key '$key' is: $ci_value"
-#             echo "Manifest value of the key '$key' is: $manifest_value"
-#         fi
-#     done
+    ###############################################################################
+    # Perform operations for global and other sections under values.yaml
+    ###############################################################################
+    keys=("global.dictionaryUrl"
+     "global.portalApp"
+     "global.netpolicy"
+     "global.frontendRoot"
+     "global.es7"
+     "google.enabled"
+     "ssjdispatcher.indexing"
+     )
+    for key in "${keys[@]}"; do
+        ci_value=$(yq eval ".$key // \"key not found\"" $cdis_manifest/values.yaml)
+        manifest_value=$(yq eval ".$key // \"key not found\"" $temp_cdis_manifest/values.yaml)
+        if [ "$manifest_value" = "key not found" ]; then
+            echo "The key '$key' is not present in the Manifest YAML file."
+        else
+            echo "CI default value of the key '$key' is: $ci_value"
+            echo "Manifest value of the key '$key' is: $manifest_value"
+        fi
+    done
 
-#     ###############################################################################
-#     # Check if manifest portal.yaml exists and perform operations on ci portal.yaml
-#     ###############################################################################
-#     if [ ! -f "$cdis_manifest/portal.yaml" ]; then
-#         cp "$temp_cdis_manifest/portal.yaml" "$cdis_manifest/portal.yaml"
-#     else
-#         if [ -f "$temp_cdis_manifest/portal.yaml" ]; then
-#             # Update the image tag
-#             image_tag_value=$(yq eval ".portal.image.tag" $temp_cdis_manifest/portal.yaml 2>/dev/null)
-#             yq eval ".portal.image.tag = \"$image_tag_value\"" -i $cdis_manifest/portal.yaml
-#             # Update the gitops json
-#             gitops_json_value=$(yq eval ".portal.gitops.json" $temp_cdis_manifest/portal.yaml 2>/dev/null)
-#             yq eval ".portal.gitops.json = \"$gitops_json_value\"" -i $cdis_manifest/portal.yaml
-#         fi
-#     fi
+    ###############################################################################
+    # Check if manifest portal.yaml exists and perform operations on ci portal.yaml
+    ###############################################################################
+    if [ ! -f "$cdis_manifest/portal.yaml" ]; then
+        cp "$temp_cdis_manifest/portal.yaml" "$cdis_manifest/portal.yaml"
+    else
+        if [ -f "$temp_cdis_manifest/portal.yaml" ]; then
+            # Update the image tag
+            image_tag_value=$(yq eval ".portal.image.tag" $temp_cdis_manifest/portal.yaml 2>/dev/null)
+            yq eval ".portal.image.tag = \"$image_tag_value\"" -i $cdis_manifest/portal.yaml
+            # Update the gitops json
+            gitops_json_value=$(yq eval ".portal.gitops.json" $temp_cdis_manifest/portal.yaml 2>/dev/null)
+            yq eval ".portal.gitops.json = \"$gitops_json_value\"" -i $cdis_manifest/portal.yaml
+        fi
+    fi
 
 
-#     ###############################################################################
-#     # Check if manifest etl.yaml exists and perform operations on ci etl.yaml
-#     ###############################################################################
-#     if [ ! -f "$cdis_manifest/etl.yaml" ]; then
-#         cp "$temp_cdis_manifest/etl.yaml" "$cdis_manifest/etl.yaml"
-#     else
-#         if [ -f "$temp_cdis_manifest/etl.yaml" ]; then
-#             # Update spark section
-#             yq eval "if (.etl.image | has(\"spark\")) then .etl.image.spark = load(\"$temp_cdis_manifest/etl.yaml\") | .etl.image.spark else . end" -i "$cdis_manifest/etl.yaml"
-#             yq eval "if (.etl.image | has(\"spark\") | not) then del(.etl.image.spark) else . end" -i "$cdis_manifest/etl.yaml"
+    ###############################################################################
+    # Check if manifest etl.yaml exists and perform operations on ci etl.yaml
+    ###############################################################################
+    if [ ! -f "$cdis_manifest/etl.yaml" ]; then
+        cp "$temp_cdis_manifest/etl.yaml" "$cdis_manifest/etl.yaml"
+    else
+        if [ -f "$temp_cdis_manifest/etl.yaml" ]; then
+            # Update spark section
+            yq eval "if (.etl.image | has(\"spark\")) then .etl.image.spark = load(\"$temp_cdis_manifest/etl.yaml\") | .etl.image.spark else . end" -i "$cdis_manifest/etl.yaml"
+            yq eval "if (.etl.image | has(\"spark\") | not) then del(.etl.image.spark) else . end" -i "$cdis_manifest/etl.yaml"
 
-#             # Update tube section
-#             yq eval "if (.etl.image | has(\"tube\")) then .etl.image.tube = load(\"$temp_cdis_manifest/etl.yaml\") | .etl.image.tube else . end" -i "$cdis_manifest/etl.yaml"
-#             yq eval "if (.etl.image | has(\"tube\") | not) then del(.etl.image.tube) else . end" -i "$cdis_manifest/etl.yaml"
-#         fi
-#     fi
+            # Update tube section
+            yq eval "if (.etl.image | has(\"tube\")) then .etl.image.tube = load(\"$temp_cdis_manifest/etl.yaml\") | .etl.image.tube else . end" -i "$cdis_manifest/etl.yaml"
+            yq eval "if (.etl.image | has(\"tube\") | not) then del(.etl.image.tube) else . end" -i "$cdis_manifest/etl.yaml"
+        fi
+    fi
 fi
 
 kubectl delete pvc -l app.kubernetes.io/name=postgresql -n ${{ env.NAMESPACE }}
