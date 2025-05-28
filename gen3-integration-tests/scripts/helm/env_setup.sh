61a22c525811267950926a77553cb6fd8ce6f174
EliseCastle23 <109446148+EliseCastle23@users.noreply.github.com>
2025-05-28 11:23:39 -0600

    debug

--- gen3-integration-tests/scripts/helm/env_setup.sh
+++ gen3-integration-tests/scripts/helm/env_setup.sh
@@ -246,6 +246,7 @@ install_helm_chart() {
   if [ "$helm_branch" != "master" ]; then
     git clone --branch "$helm_branch" https://github.com/uc-cdis/gen3-helm.git
     helm dependency update gen3-helm/helm/gen3
+    cat $ci_default_manifest_values_yaml | grep -i "elasticsearch:"
     if helm upgrade --install ${namespace} gen3-helm/helm/gen3 --set global.hostname="${HOSTNAME}" -f $ci_default_manifest_values_yaml -n "${NAMESPACE}"; then
       echo "Helm chart installed!"
     else
