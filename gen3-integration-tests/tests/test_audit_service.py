f264f97274b7cd43cd9690240b2fbf4aac31ba87
Krishna Agarwal <krishnaa@uchicago.edu>
2025-06-16 12:42:45 -0500

    Skip OIDC for non-nightly build PR

--- gen3-integration-tests/tests/test_audit_service.py
+++ gen3-integration-tests/tests/test_audit_service.py
@@ -127,6 +127,11 @@ class TestAuditService:
             "login", "smarty_two", params, expected_results
         )
 
+    @pytest.mark.skipif(
+        "nightly-build" not in pytest.hostname
+        and os.getenv("GEN3_INSTANCE_TYPE") == "HELM_LOCAL",
+        reason="Test is being run on Helm and would run only on nightly-build",
+    )
     def test_audit_oidc_login_events(self, page: Page):
         """
         Scenario: Perform login using ORCID and validate audit entry
