4578aec450c4fc5c8427ed7eee9110d8883d51ab
Hara Prasad Juvvala <haraprasadj@uchicago.edu>
2025-07-15 12:15:29 -0500

    Fix env setup for tube PRs

--- gen3-integration-tests/scripts/helm/env_setup.sh
+++ gen3-integration-tests/scripts/helm/env_setup.sh
@@ -42,7 +42,10 @@ elif [ "$setup_type" == "service-env-setup" ]; then
     service_values_block=$(yq eval ".${service_name} // \"key not found\"" "$ci_default_manifest_values_yaml")
     if [ "$service_values_block" != "key not found" ]; then
         echo "Key '$service_name' found in \"$ci_default_manifest_values_yaml.\""
-        yq eval ".${service_name}.image.tag = \"${image_name}\"" -i "$ci_default_manifest_values_yaml"
+        if [[ "$service" == "etl" ]]; then
+          yq eval ".etl.image.tube.tag = \"${image_name}\"" -i "$ci_default_manifest_values_yaml"
+        else
+          yq eval ".${service_name}.image.tag = \"${image_name}\"" -i "$ci_default_manifest_values_yaml"
     elif [ "$service_yaml_block" != "key not found" ]; then
         echo "Key '$service_name' not found.\""
         exit 1
