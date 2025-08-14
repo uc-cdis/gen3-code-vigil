4ae70890a83832d67d24a40db79ab702600747c2
krishna <krishnaa@uchicago.edu>
2025-08-14 09:35:53 -0500

    Delete indexd-userdb for nightly-build and nightly-build to helm-ci-cleanup script

--- gen3-integration-tests/scripts/helm/env_setup.sh
+++ gen3-integration-tests/scripts/helm/env_setup.sh
@@ -413,6 +413,10 @@ touch $ci_default_manifest_portal_yaml
 yq eval-all 'select(fileIndex == 0) * {"portal": select(fileIndex == 1).portal}' $ci_default_manifest_portal_yaml $ci_default_manifest_values_yaml -i
 yq eval 'del(.portal)' $ci_default_manifest_values_yaml -i
 
+if [ "$namespace" == "nightly-build" ]; then
+  kubectl delete job indexd-userdb -n $namespace
+fi
+
 
 echo $HOSTNAME
 install_helm_chart() {
