8c8ec17177c50e8aa233cee6f8fa6e7187c573ca
krishna <krishnaa@uchicago.edu>
2025-08-20 08:23:06 -0500

    Print sower block

--- gen3-integration-tests/gen3_ci/scripts/setup_ci_env.sh
+++ gen3-integration-tests/gen3_ci/scripts/setup_ci_env.sh
@@ -165,6 +165,7 @@ elif [ "$setup_type" == "manifest-env-setup" ]; then
         yq eval '.sower.externalSecrets.createK8sPelicanServiceSecret = false' -i "$ci_default_manifest_values_yaml"
         yq eval '.sower.externalSecrets.pelicanserviceG3auto = ci-pelicanservice-g3auto' -i "$ci_default_manifest_values_yaml"
         yq eval '.sower.externalSecrets.createK8sSowerJobsSecret = true' -i "$ci_default_manifest_values_yaml"
+        yq eval '.sower' "$ci_default_manifest_values_yaml"
     fi
 
     ####################################################################################
