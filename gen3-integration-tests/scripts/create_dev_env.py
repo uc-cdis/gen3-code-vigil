27fec3d78394303f0dfc353c44625725d63e1ca6
krishna <krishnaa@uchicago.edu>
2025-09-11 12:57:30 -0500

    Print generate api key cmd

--- gen3-integration-tests/scripts/create_dev_env.py
+++ gen3-integration-tests/scripts/create_dev_env.py
@@ -29,6 +29,7 @@ def generate_api_keys_for_test_users():
         os.getenv("HOSTNAME"),
         os.getenv("NAMESPACE"),
     ]
+    logger.info(cmd)
     result = subprocess.run(
         cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
     )
