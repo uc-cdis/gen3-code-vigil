d52c3ebb59514c67b130ebbc5320f299256f1514
Krishna Agarwal <159047652+krishnaa05@users.noreply.github.com>
2025-10-02 12:34:54 -0500

    Update test_aggregate_mds.py

--- gen3-integration-tests/tests/test_aggregate_mds.py
+++ gen3-integration-tests/tests/test_aggregate_mds.py
@@ -7,7 +7,7 @@ from services.metadataservice import MetadataService
 from utils import TEST_DATA_PATH_OBJECT, logger
 from utils.test_execution import assert_with_retry
 
-
+#testing
 @pytest.mark.skipif(
     "metadata" not in pytest.deployed_services,
     reason="Metadata service is not running on this environment",
