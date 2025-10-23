981ae3445568adeed5cfc7a9c5c4bc01e98033d2
krishna <krishnaa@uchicago.edu>
2025-10-23 11:30:46 -0500

    Add Env Sanity Test for Version Check

---
+++ gen3-integration-tests/tests/test_env_sanity.py
@@ -0,0 +1,61 @@
+"""
+ENV SANITY
+"""
+
+import os
+
+import pytest
+from gen3.auth import Gen3Auth
+from utils import logger
+
+
+@pytest.mark.env_sanity
+@pytest.mark.skipif(
+    "chore/apply_" not in os.getenv("BRANCH"),
+    reason="Current PR is not a release PR",
+)
+class TestEnvSanity:
+    @classmethod
+    def setup_class(cls):
+        cls.BASE_URL = f"{pytest.root_url}"
+
+    def test_service_versions(self):
+        """
+        Scenario: Test service versions
+        Steps:
+            1. Get list of deployments on the current namespace
+            2. Get the service version by calling the service version endpoint
+            3. Validate the service version matches the version of the deployment
+        """
+        logger.info("Running Env Sanity Test")
+        release_version = os.getenv("BRANCH").split("_")[1]
+        service_endpoints = {
+            "audit": "/audit/_version",
+            "fence": "/user/_version",
+            "gen3-user-data-library": "/library/_version",
+            "guppy": "/guppy/_version",
+            "indexd": "/index/_version",
+            "metadata-service": "/mds/version",  # version
+            "peregrine": "/peregrine/_version",
+            "requestor": "/requestor/_version",
+            "sheepdog": "/api/_version",
+            "wts": "/wts/_version",
+        }
+        for service in pytest.deployed_services:
+            service_name = service.replace("-deployment", "")
+            if service_name in service_endpoints.keys:
+                logger.info(f"Service {service_name} found, checking service version")
+                auth = Gen3Auth(
+                    refresh_token=pytest.api_keys["main_account"],
+                    endpoint=pytest.root_url,
+                )
+                url = self.BASE_ENDPOINT + "/_status"
+                response = auth.curl(path=url)
+                assert (
+                    response.status_code == 200
+                ), f"Expected 200 but got {response.status_code}"
+                data = response.json()
+                logger.info(f"Got version {data['version']}")
+                assert (
+                    data["version"] == release_version
+                ), f"Expected {release_version} but got {data['version']}"
