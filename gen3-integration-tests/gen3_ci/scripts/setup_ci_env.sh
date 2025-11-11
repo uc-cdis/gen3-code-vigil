274d4c1d0a9156d4382db6dc16bd5452dcc4b855
Sai Shanmukha <nss10@outlook.com>
2025-11-11 16:35:27 -0600

    Add echo before aws secretmanager delete

--- gen3-integration-tests/gen3_ci/scripts/setup_ci_env.sh
+++ gen3-integration-tests/gen3_ci/scripts/setup_ci_env.sh
@@ -441,6 +441,7 @@ if [ "$namespace" == "nightly-build" ]; then
 fi
 
 # Ensure funnel-oidc-client for this namespace does not exist in secrets manager before installing the helm chart
+echo "Deleting $namespace-funnel-oidc-client from aws secrets manager, if it exists"
 aws secretsmanager delete-secret --secret-id $namespace-funnel-oidc-client --force-delete-without-recovery
 
 echo $HOSTNAME
