4388f4bf8800126354402c3372f9ddf5550f707b
krishna <krishnaa@uchicago.edu>
2025-12-18 15:50:57 -0600

    Discovery change

--- gen3-integration-tests/tests/test_discoverypage.py
+++ gen3-integration-tests/tests/test_discoverypage.py
@@ -41,10 +41,10 @@ def page_setup(page):
     "discoveryConfig" not in portal_config and "metadataConfig" not in portal_config,
     reason="Neither discoveryConfig nor metadataConfig found in portal config",
 )
-@pytest.mark.skipif(
-    "batch-export" not in pytest.enabled_sower_jobs,
-    reason="batch-export is not part of sower in manifest",
-)
+# @pytest.mark.skipif(
+#     "batch-export" not in pytest.enabled_sower_jobs,
+#     reason="batch-export is not part of sower in manifest",
+# )
 @pytest.mark.workspace
 @pytest.mark.mds
 @pytest.mark.agg_mds
@@ -62,15 +62,15 @@ class TestDiscoveryPage(object):
             "694b1d13b8148756442739fa2cc37fd6"  # pragma: allowlist secret
         )
 
-    @classmethod
-    def teardown_class(cls):
-        logger.info(
-            "Tearing down - delete indexd record and study metadata, and terminate workspace"
-        )
-        indexd = Indexd()
-        mds = MetadataService()
-        indexd.delete_records([cls.variables["did"]])
-        mds.delete_metadata(cls.variables["study_id"])
+    # @classmethod
+    # def teardown_class(cls):
+    #     logger.info(
+    #         "Tearing down - delete indexd record and study metadata, and terminate workspace"
+    #     )
+    #     indexd = Indexd()
+    #     mds = MetadataService()
+    #     indexd.delete_records([cls.variables["did"]])
+    #     mds.delete_metadata(cls.variables["study_id"])
 
     def test_study_publish_search_export(self, page_setup):
         """
@@ -153,7 +153,7 @@ class TestDiscoveryPage(object):
         # Tag search
         # TODO: Tag columns are not yet added to frontend framework (GFF-522)
         if not pytest.frontend_url:
-            discovery_page.search_tag(page_setup, self.variables["study_id"])
+            discovery_page.search_tag(page_setup, "AUTOTEST Tag")
             screenshot(page_setup, "TagSearch")
             assert discovery_page.study_found(page_setup, "AUTOTEST Tag")
             screenshot(page_setup, "StudyFound")
