b0bd02201bec2537e2f81feaac9de0a0c57a2810
krishna <krishnaa@uchicago.edu>
2025-12-23 10:09:49 -0600

    Login fix for accept button

--- gen3-integration-tests/pages/login.py
+++ gen3-integration-tests/pages/login.py
@@ -32,7 +32,7 @@ class LoginPage(object):
         )
         self.LOGOUT_LOCATOR = re.compile("Logout", re.IGNORECASE)
         self.POP_UP_BOX = "//div[@class='popup__box']"  # pop_up_box
-        self.POP_UP_ACCEPT_BUTTON = "//button[contains(text(),'Accept')]"
+        self.POP_UP_ACCEPT_BUTTON = "//button[contains(normalize-space(),'Accept')]"
         self.RAS_SIGN_IN_BUTTON = "//button[contains(text(),'Sign in')]"
         self.RAS_USERNAME_INPUT = "//input[@id='USER']"
         self.RAS_PASSWORD_INPUT = "//input[@id='PASSWORD']"
