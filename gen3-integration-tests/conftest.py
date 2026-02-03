b8022442e02bc3624ae6c1ee0593d3963ccdd6ed
krishna <krishnaa@uchicago.edu>
2026-02-03 11:29:34 -0600

    Add try

--- gen3-integration-tests/conftest.py
+++ gen3-integration-tests/conftest.py
@@ -167,6 +167,9 @@ def pytest_configure(config):
 
 
 def pytest_runtest_logreport(report):
+    if report.when != "call":
+        return
+
     test_nodeid = report.nodeid
     start_time = datetime.fromtimestamp(report.start)
     message = {
@@ -181,10 +184,7 @@ def pytest_runtest_logreport(report):
         "duration": str(timedelta(seconds=report.duration)),
     }
     # Collect test suite failures for re-run
-    if (
-        report.failed
-        and test_nodeid.split("::")[1] not in failed_test_suites
-    ):
+    if report.failed and test_nodeid.split("::")[1] not in failed_test_suites:
         failed_test_suites.append(test_nodeid.split("::")[1])
     # Add data to the queue
     try:
