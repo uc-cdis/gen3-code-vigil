579a79cfa7d59bf831fa1dc1f8fbd8e86946f9a1
Krishna Agarwal <159047652+krishnaa05@users.noreply.github.com>
2026-02-13 09:06:48 -0600

    Debug FF repository issue on helm

--- gen3-integration-tests/gen3_ci/scripts/setup_ci_env.sh
+++ gen3-integration-tests/gen3_ci/scripts/setup_ci_env.sh
@@ -226,6 +226,7 @@ elif [ "$setup_type" == "manifest-env-setup" ]; then
             yq eval ".${key}.image.tag = \"$image_tag_value\"" -i $ci_default_manifest_values_yaml
             if [[ "$key" == "frontend-framework" ]]; then
               repository=$(yq eval ".frontend-framework.image.repository // \"key not found\"" "$new_manifest_values_file_path")
+              echo "Updating ${key} image reposiotry with ${repository}"
               yq eval ".frontend-framework.image.repository = \"${repository}" -i "$ci_default_manifest_values_yaml"
             fi
         fi
