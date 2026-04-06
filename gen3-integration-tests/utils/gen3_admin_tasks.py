6229053f06274415dc2840c1a6a824f5566f7dea
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2026-04-06 12:45:35 -0500

    run useryaml job instead of usersync

--- gen3-integration-tests/utils/gen3_admin_tasks.py
+++ gen3-integration-tests/utils/gen3_admin_tasks.py
@@ -113,9 +113,29 @@ def run_gen3_job(
     """
     Run gen3 job (e.g., metadata-aggregate-sync).
     """
+
+    logger.info("-------- run_gen3_job useryaml")
+    cmd = ["kubectl", "-n", test_env_namespace, "get", "job", job_name]
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if result.returncode == 0:
+        logger.info(f"success - {result.stdout.decode('utf-8')}")
+    else:
+        logger.info(f"failure - {result.returncode} - {result.stderr.decode('utf-8')}")
+
+    # logger.info("-------- run_gen3_job useryaml")
+    # cmd = [
+    #     f"kubectl -n {test_env_namespace} get job {job_name} -o json | jq 'del(.spec.selector, .spec.template.metadata.labels, .status, .metadata.uid, .metadata.resourceVersion, .metadata.creationTimestamp)'"
+    # ]
+    # result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # if result.returncode == 0:
+    #     logger.info(f"success - {result.stdout.decode('utf-8')}")
+    # else:
+    #     logger.info(f"failure - {result.returncode} - {result.stderr.decode('utf-8')}")
+    # logger.info("-------- run_gen3_job useryaml")
+
     if job_name == "useryaml":  # this is a job, not a cronjob
         cmd = [
-            f"kubectl -n {test_env_namespace} get job {job_name} -o json | jq 'del(.spec.selector, .spec.template.metadata.labels, .status, .metadata.uid, .metadata.resourceVersion, .metadata.creationTimestamp)' | kubectl -n {test_env_namespace} replace --force -f -"
+            f"kubectl -n {test_env_namespace} get job {job_name} -o json | jq 'del(.spec.selector, .spec.template.metadata.labels, .status, .metadata.uid, .metadata.resourceVersion, .metadata.creationTimestamp)' | kubectl -n {test_env_namespace} replace --force -f -"  # TODO try without force
         ]
     else:
         cronjob_name = job_name
@@ -133,12 +153,30 @@ def run_gen3_job(
             f"--from=cronjob/{cronjob_name}",
             job_name,
         ]
-    logger.info(cmd)
+    logger.info(f"Running command: {cmd}")
     result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if result.returncode == 0:
         logger.info(f"{job_name} job triggered - {result.stdout.decode('utf-8')}")
     else:
-        raise Exception(f"{job_name} failed to start - {result.stderr.decode('utf-8')}")
+        logger.info(f"{job_name} failed to start - {result.stderr.decode('utf-8')}")
+        logger.info("-------- run_gen3_job retrying with new syntax")
+        cmd = (
+            f"kubectl -n {test_env_namespace} get job {job_name} -o json "
+            "| jq 'del(.spec.selector, .spec.template.metadata.labels, .status, .metadata.uid, .metadata.resourceVersion, .metadata.creationTimestamp)' "
+            f"> /tmp/{job_name}.json "
+            f"&& kubectl -n {test_env_namespace} delete -f /tmp/{job_name}.json "
+            f"&& kubectl -n {test_env_namespace} apply -f /tmp/{job_name}.json"
+        )
+        logger.info(f"Running command: {cmd}")
+        result = subprocess.run(
+            cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        if result.returncode == 0:
+            logger.info(f"{job_name} job triggered - {result.stdout.decode('utf-8')}")
+        else:
+            raise Exception(
+                f"{job_name} failed to start - {result.stderr.decode('utf-8')}"
+            )
     # TODO fix this...need job_name
     # check_job_pod(job_name=job_name, test_env_namespace=pytest.namespace)
     import time
