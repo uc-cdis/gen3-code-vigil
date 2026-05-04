20bb83b40531ed7c05c7ac970d34fa5c273a1f04
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2026-05-04 15:42:51 -0500

    MIDRC-1171 Support running gen3-workflow tests in a Kind cluster (#541)

--- gen3-integration-tests/conftest.py
+++ gen3-integration-tests/conftest.py
@@ -10,6 +10,7 @@ import pytest
 
 # Using dotenv to simplify setting up env vars locally
 from dotenv import load_dotenv
+from gen3_ci.scripts.prepare_ci_environment import generate_api_keys_for_test_users
 from utils import TEST_DATA_PATH_OBJECT
 from utils import gen3_admin_tasks as gat
 from utils import logger
@@ -96,6 +97,10 @@ def get_fence_clients():
 
 
 def pytest_configure(config):
+    # generate api keys for test users for the ci env
+    result = generate_api_keys_for_test_users()
+    assert result.lower() == "success"
+
     # Compute hostname and namespace
     pytest.hostname = os.getenv("HOSTNAME")
     pytest.namespace = os.getenv("NAMESPACE")
@@ -105,7 +110,8 @@ def pytest_configure(config):
     if not pytest.tested_env:
         pytest.tested_env = pytest.namespace
     # Compute root_url
-    pytest.root_url = f"https://{pytest.hostname}"
+    pytest.root_url = f"{os.getenv("HOSTNAME_PROTOCOL")}://{pytest.hostname}"
+    logger.info(f"pytest root_url: '{pytest.root_url}'")
 
     # Clients used for testing
     pytest.clients = {}
