3d4d4b034a944c6cb3f59324e2ab447c147bcd98
Krishna Agarwal <159047652+krishnaa05@users.noreply.github.com>
2026-05-18 10:35:21 -0500

    Dont Merge - Testing nightly-build ff portal issue

--- gen3-integration-tests/gen3_ci/scripts/setup_ci_env.sh
+++ gen3-integration-tests/gen3_ci/scripts/setup_ci_env.sh
@@ -514,9 +514,11 @@ fi
 # so env doesnt need portal configuration
 if [[ "$setup_type" == "test-env-setup" || "$setup_type" == "service-env-setup" || ("$setup_type" == "manifest-env-setup"  && "$SOURCE_CONFIG" == "ci/default") ]]; then
   if [[ "$CI_ENV" == "gen3ff" ]]; then
+    echo "Deleting portal service"
     yq eval 'del(.portal)' --inplace "$ci_default_manifest_values_yaml"
     yq eval '.global.frontendRoot = "gen3ff"' --inplace "$ci_default_manifest_values_yaml"
   else
+    echo "Deleting frontend service"
     yq eval 'del(.frontend-framework)' --inplace "$ci_default_manifest_values_yaml"
   fi
 fi
