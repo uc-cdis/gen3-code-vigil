843f643628053481324d4d4e649a4454b80bf6c5
Krishna Agarwal <159047652+krishnaa05@users.noreply.github.com>
2026-06-04 10:11:00 -0500

    Fix dev env setup issue for portal (#567)
    * Fix dev env setup issue for portal
    
    * minor change

--- gen3-integration-tests/utils/gen3_admin_tasks.py
+++ gen3-integration-tests/utils/gen3_admin_tasks.py
@@ -1181,14 +1181,14 @@ def service_version_greater_than(service_name, min_release_version, min_sem_veri
     logger.info(f"MinVersion: {min_release_version}")
     try:
         parsed_current = Version(current_version)
+        CALVER_RE = re.compile(r"^\d{4}\.\d{2}(\.\d+)?$")
+        if CALVER_RE.match(current_version):
+            # CALVER version
+            return Version(min_release_version) >= parsed_current
+        else:
+            # SEMVER version
+            return Version(min_sem_verion) >= parsed_current
     except InvalidVersion:
         # If the branch is master/main/branch with aphabets,
         # it will return False to execute the test
         return False
-    CALVER_RE = re.compile(r"^\d{4}\.\d{2}(\.\d+)?$")
-    if CALVER_RE.match(current_version):
-        # CALVER version
-        return Version(min_release_version) >= parsed_current
-    else:
-        # SEMVER version
-        return Version(min_sem_verion) >= parsed_current
