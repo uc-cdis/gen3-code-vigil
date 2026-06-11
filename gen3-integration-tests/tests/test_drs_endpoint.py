906fe4ddd59d84588d61fa0b6493dc005842e0fc
krishna <krishnaa@uchicago.edu>
2026-06-11 09:59:47 -0500

    DRS changes

--- gen3-integration-tests/tests/test_drs_endpoint.py
+++ gen3-integration-tests/tests/test_drs_endpoint.py
@@ -2,12 +2,7 @@
 DRS Endpoint
 """
 
-import os
-
 import pytest
-from gen3.auth import Gen3Auth
-from gen3.tools.download.drs_download import list_drs_object
-from packaging.version import Version
 from services.drs import Drs
 from services.fence import Fence
 from services.indexd import Indexd
@@ -61,17 +56,18 @@ class TestDrsEndpoints:
         # Removing test indexd records
         cls.indexd.delete_records(cls.variables["created_indexd_dids"])
 
-    @pytest.mark.gen3sdk
     def test_get_drs_object(self):
         """
         Scenario: get drs object
         Steps:
             1. Get the drs object for indexd record (allowed).
-            2. Confirm the drs object is present
+            2. Get the drs object and compare the records are same.
         """
-        res = self.drs.get_drs_object_using_gen3sdk(indexd_files["allowed"])
-        logger.info(res)
-        assert res, f"Expected drs object to be present but got {res}"
+        drs_record = self.drs.get_drs_object(file=indexd_files["allowed"])
+        res = self.drs.get_drs_object(drs_record.json())
+        assert (
+            drs_record.json() == res.json()
+        ), f"Expected same values but got different.\ndrs_record: {drs_record}\nResponse: {res}"
 
     def test_get_drs_no_record_found(self):
         """
@@ -80,10 +76,7 @@ class TestDrsEndpoints:
             1. Get the drs object for indexd record (not_allowed).
             2. Get the drs object usign the response object in step 1. Drs object shouldn't be returned.
         """
-        drs_record = self.drs.get_drs_object_using_gen3sdk(
-            file=indexd_files["not_allowed"]
-        )
-        logger.info(drs_record)
+        drs_record = self.drs.get_drs_object(file=indexd_files["not_allowed"])
         res = self.drs.get_drs_object(drs_record)
         assert (
             res.status_code == 404
@@ -142,15 +135,3 @@ class TestDrsEndpoints:
         assert (
             file_content == expected_content
         ), f"Data don't match.\n{file_content}\n{expected_content}"
-
-    @pytest.mark.gen3sdk
-    def test_dowload_drs_object_invalid_access_id(self):
-        """
-        Scenario: download drs object
-        Steps:
-            1. Download drs object for indexd record (invalid_protocol).
-            2. Validate the content of the file checkout.
-        """
-        did = indexd_files["invalid_protocol"]["did"]
-        response = self.drs.get_drs_download(file=indexd_files["invalid_protocol"])
-        logger.info(response)
