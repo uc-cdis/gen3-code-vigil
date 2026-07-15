2f27e5f5c446039524ad8b58e66d2558f8d0eaab
krishna <krishnaa@uchicago.edu>
2026-07-15 10:51:44 -0500

    Add test for crud operations using non-admin priviledged user

--- gen3-integration-tests/tests/test_gen3_embedding.py
+++ gen3-integration-tests/tests/test_gen3_embedding.py
@@ -138,3 +138,128 @@ class TestGen3Embedding:
         assert (
             response.status_code == 401
         ), f"Expected status to be 401 but got {response.status_code}"
+
+    def test_crud_operations_non_admin_privileged_user(self):
+        """
+        Scenario: A non-admin privileged user can perform only read operation
+        Steps:
+            1. Create a collection named public using indexing_account
+            2. Verify indexing_account can't create the collection
+            3. Create a collection named public using main_account
+            4. Verify indexing_account can't update the collection
+            5. Verify indexing_account can read the collection
+            6. Verify indexing_account can't delete the collection
+            7. Create embeddings in collection public using indexing_account
+            8. Verify indexing_account can't create the embedding
+            9. Create embeddings in collection public using main_account
+            10. Verify indexing_account can't update the embedding
+            11. Verify indexing_account can read the embedding
+            12. Verify indexing_account can't delete the embedding
+        """
+        try:
+            # Create the collection with user without admin privileges
+            response = self.gen3_embedding.create_collection(
+                data=self.collection_data["public"], user="indexing_account"
+            )
+            assert (
+                response.status_code == 401
+            ), f"Expected status to be 401 but got {response.status_code}"
+            # Create the collection with user having admin privileges
+            response = self.gen3_embedding.create_collection(
+                data=self.collection_data["public"]
+            )
+            assert (
+                response.status_code == 200
+            ), f"Expected status to be 200 but got {response.status_code}"
+            # Update the collection with user without admin privileges
+            response = self.gen3_embedding.update_collection(
+                collection_name="public",
+                data=self.updated_collection_data["public"],
+                user="indexing_account",
+            )
+            assert (
+                response.status_code == 403
+            ), f"Expected status to be 403 but got {response.status_code}"
+            # Get the collection with user without admin privileges
+            response = self.gen3_embedding.get_collection(
+                collection_name="public", user="indexing_account"
+            )
+            assert (
+                response["description"] == self.collection_data["public"]["description"]
+            ), f"Updation failed, got response: {response}"
+            # Delete the collection with user without admin privileges
+            response = self.gen3_embedding.delete_collection(
+                collection_name="public", user="indexing_account"
+            )
+            assert (
+                response.status_code == 403
+            ), f"Expected status to be 403 but got {response.status_code}"
+            # Create Embedding with user without admin privileges
+            embedding_data = {
+                "embeddings": [
+                    {
+                        "embedding": self.embeddings[0].tolist(),
+                        "metadata": {"source": "some_file.md", "chunk_size": "1000"},
+                    }
+                ]
+            }
+            response = self.gen3_embedding.create_embedding(
+                collection_name="public", data=embedding_data, user="indexing_account"
+            )
+            assert (
+                response.status_code == 403
+            ), f"Expected status to be 403 but got {response.status_code}"
+            # Create Embedding with user having admin privileges
+            response = self.gen3_embedding.create_embedding(
+                collection_name="public", data=embedding_data
+            )
+            assert (
+                response.status_code == 200
+            ), f"Expected status to be 200 but got {response.status_code}"
+            # Update Embedding with user without admin privileges
+            updated_embedding_data = {
+                "embeddings": [
+                    {
+                        "embedding": self.embeddings[0].tolist(),
+                        "metadata": {
+                            "source": "some_file_update.md",
+                            "chunk_size": "1000",
+                        },
+                    }
+                ]
+            }
+            response = self.gen3_embedding.update_embedding(
+                collection_name="public",
+                data=updated_embedding_data,
+                user="indexing_account",
+            )
+            assert (
+                response.status_code == 403
+            ), f"Expected status to be 403 but got {response.status_code}"
+            # Get the embeddings
+            response = self.gen3_embedding.get_embedding(
+                collection_name="public", user="indexing_account"
+            )
+            assert (
+                len(response["embeddings"]) == 1
+            ), f"Expected 1 embeddings but got {len(response["embeddings"])}"
+            # Delete the embeddings without admin privileges
+            for embedding in response["embeddings"]:
+                embedding_id = embedding["embedding_id"]
+                response = self.gen3_embedding.delete_embedding(
+                    collection_name="public",
+                    embedding_id=embedding_id,
+                    user="indexing_account",
+                )
+                logger.info(response)
+                assert (
+                    response.status_code == 403
+                ), f"Expected status to be 403 but got {response.status_code}"
+        except Exception as e:
+            raise Exception(f"Got exception: {e}")
+        finally:
+            # Delete the collection
+            response = self.gen3_embedding.delete_collection(collection_name="public")
+            assert (
+                response.status_code == 204
+            ), f"Expected status to be 204 but got {response.status_code}"
