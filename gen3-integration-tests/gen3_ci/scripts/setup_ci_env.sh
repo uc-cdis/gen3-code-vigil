4854998c2e076950c9e3a6f7099f06d21906938c
Krishna Agarwal <159047652+krishnaa05@users.noreply.github.com>
2026-07-28 10:33:06 -0500

    Update setup_ci_env.sh

--- gen3-integration-tests/gen3_ci/scripts/setup_ci_env.sh
+++ gen3-integration-tests/gen3_ci/scripts/setup_ci_env.sh
@@ -354,7 +354,7 @@ elif [ "$setup_type" == "manifest-env-setup" ]; then
     yq eval 'del(."mutatingWebhook", ."neuvector", ."dashboard", ."access-backend")' -i "$ci_default_manifest_values_yaml"
 
     # Remove workspace-proxy. This is temporary until we add workspace-proxy to CI
-    yq eval "del(.workspace-proxy)" -i $ci_default_manifest_values_yaml
+    yq eval 'del(."workspace-proxy")' -i $ci_default_manifest_values_yaml
 fi
 
 # Check whether specific services are enabled in the final manifest
