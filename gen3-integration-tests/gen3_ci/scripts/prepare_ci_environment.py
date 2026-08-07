65eb7b3c31f4c443d64de81e0b609ad49bfd9779
krishna <krishnaa@uchicago.edu>
2026-08-07 12:40:51 -0500

    Fix

--- gen3-integration-tests/gen3_ci/scripts/prepare_ci_environment.py
+++ gen3-integration-tests/gen3_ci/scripts/prepare_ci_environment.py
@@ -33,13 +33,13 @@ def wait_for_quay_build(repo, tag):
     found = False
     i = 0
     repo_list = repo.split(",")
-    # Check if repo name is in dict then fetch the quay repo name
-    repo_dict = {"gen3-ai": "gen3_embeddings"}
-    if repo in repo_dict.keys():
-        repo = repo_dict["repo"]
     logger.info(f"[wait_for_quay_build] Repo - {quay_org}/{repo}, image - {tag}")
     while not found and i < max_tries:
         for repo_item in repo_list:
+            # Check if repo name is in dict then fetch the quay repo name
+            repo_dict = {"gen3-ai": "gen3_embeddings"}
+            if repo_item in repo_dict.keys():
+                repo_item = repo_dict["repo"]
             logger.info(
                 f"[wait_for_quay_build] Waiting for image '{quay_org}/{repo_item}:{tag}' to be built in quay"
             )
