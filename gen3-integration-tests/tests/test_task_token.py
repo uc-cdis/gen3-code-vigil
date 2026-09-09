9958e220159c6d727f17a9b0f0e6fdd52915b837
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2026-09-09 16:44:35 -0500

    wip convert other task token tests to use dpop

--- gen3-integration-tests/tests/test_task_token.py
+++ gen3-integration-tests/tests/test_task_token.py
@@ -13,32 +13,46 @@ JA4 enforcement
   - with `curl`
 - change the JA4 (how?)
   - then check that the TES server rejects the token
+
+TODO update the other gen3-workflow tests
+TODO disable when fence or gen3-workflow version is too low
 """
 
+import tempfile
 import time
+from contextlib import contextmanager
 
-# import gen3
-from gen3.auth import Gen3Auth
-from gen3.dpop import dpop_proxy_context, resolve_service_endpoints
 import jwt
 import pytest
 import requests
-import tempfile
 
+# import gen3
+from gen3.auth import Gen3Auth
+from gen3.dpop import dpop_proxy_context
 from services.fence import Fence
-from services.gen3workflow import Gen3Workflow, WorkflowStorageConfig, nextflow_parse_completed_line, nextflow_parse_completed_tasks
+from services.gen3workflow import (
+    Gen3Workflow,
+    WorkflowStorageConfig,
+    nextflow_parse_completed_line,
+    nextflow_parse_completed_tasks,
+)
 from utils import logger
 
 
-# TODO rename or comment: non-dpop
+@contextmanager
 def get_task_token(
     type="WORKFLOW", user="main_account", expires_in=3600, expected_status_code=200
 ):
-    url = f"{pytest.root_url}/user/credentials/api/access_token?task_token={type}&expires_in={expires_in}"
-    res = requests.post(url, json={"api_key": pytest.api_keys[user]["api_key"]})
-    assert res.status_code == expected_status_code, res.text
-    if res.status_code == 200:
-        return res.json()["access_token"]
+    auth = Gen3Auth(refresh_token=pytest.api_keys[user], endpoint=pytest.root_url)
+    try:
+        with dpop_proxy_context(
+            auth=auth, task_token_type=type, task_token_expiration=expires_in
+        ) as (task_token, proxy_port):
+            yield task_token, proxy_port
+    except Exception as e:
+        if expected_status_code == 200:
+            raise
+        assert f"[{expected_status_code}]" in str(e)
 
 
 @pytest.mark.skipif(
@@ -51,7 +65,9 @@ class TestTaskToken(object):
     def setup_class(cls):
         cls.fence = Fence()
         cls.gen3_workflow = Gen3Workflow()
-        cls.s3_storage_config = WorkflowStorageConfig.from_dict(cls.gen3_workflow.setup_storage())
+        cls.s3_storage_config = WorkflowStorageConfig.from_dict(
+            cls.gen3_workflow.setup_storage()
+        )
 
     def test_obtain_task_token(self):
         """
@@ -61,8 +77,8 @@ class TestTaskToken(object):
         # request a WORKFLOW task token > 3600 and <= 4000
         default_max_exp = 3600
         requested_exp = 3777
+        workflow_task_token, _ = get_task_token(expires_in=requested_exp)
         now = int(time.time())
-        workflow_task_token = get_task_token(expires_in=requested_exp)
         exp = jwt.decode(
             workflow_task_token,
             algorithms=["RS256"],
@@ -72,7 +88,8 @@ class TestTaskToken(object):
 
         # the longer lifetime should not work for task token type != WORKFLOW since it's not
         # configured. We should get exp == MAX_ACCESS_TOKEN_TTL
-        other_task_token = get_task_token("FOO", expires_in=requested_exp)
+        other_task_token, _ = get_task_token("FOO", expires_in=requested_exp)
+        now = int(time.time())
         exp = jwt.decode(
             other_task_token, algorithms=["RS256"], options={"verify_signature": False}
         )["exp"]
@@ -90,45 +107,46 @@ class TestTaskToken(object):
         TODO
         """
         user = "main_account"
-        regular_token = self.gen3_workflow.get_access_token(user)
-        workflow_task_token = get_task_token()
-        other_task_token = get_task_token("FOO")
-
-        # fail to use a WORKFLOW task token on a non-WORKFLOW endpoint in Fence
-        url = f"{pytest.root_url}/user/user"
-        res = requests.get(
-            url, headers={"Authorization": f"bearer {workflow_task_token}"}
-        )
-        assert res.status_code == 401, res.text
-        assert "token audience validation failed" in res.text
-
-        # fail to use a WORKFLOW task token on a non-WORKFLOW endpoint outside of Fence
-        # TODO something else than arborist
-        # url = f"{pytest.root_url}/authz/mapping"
-        # res = requests.get(url, headers={"Authorization": f"bearer {workflow_task_token}"})
-        # assert res.status_code == 401, res.text
-        # assert "token audience validation failed" in res.text
 
         # fail to use a non-task token on a WORKFLOW endpoint
+        regular_token = self.gen3_workflow.get_access_token(user)
         # TODO uncomment once gen3-workflow is updated to reject non-task tokens
         # url = f"{pytest.root_url}/ga4gh/tes/v1/tasks"
         # res = requests.get(url, headers={"Authorization": f"bearer {regular_token}"})
         # assert res.status_code == 401, res.text
         # assert "token audience validation failed" in res.text
 
-        # fail to use a non-WORKFLOW task token on a WORKFLOW endpoint
-        # TODO uncomment once gen3-workflow is updated to reject non-WORKFLOW task tokens
-        # url = f"{pytest.root_url}/ga4gh/tes/v1/tasks"
-        # res = requests.get(url, headers={"Authorization": f"bearer {other_task_token}"})
-        # assert res.status_code == 401, res.text
-        # assert "token audience validation failed" in res.text
+        with get_task_token() as (workflow_task_token, proxy_port):
+            # fail to use a WORKFLOW task token on a non-WORKFLOW endpoint in Fence
+            url = f"{pytest.root_url}/user/user"
+            res = requests.get(
+                url, headers={"Authorization": f"bearer {workflow_task_token}"}
+            )
+            assert res.status_code == 401, res.text
+            assert "token audience validation failed" in res.text
 
-        # succeed using a WORKFLOW task token on a WORKFLOW endpoint
-        url = f"{pytest.root_url}/ga4gh/tes/v1/tasks"
-        res = requests.get(
-            url, headers={"Authorization": f"bearer {workflow_task_token}"}
-        )
-        assert res.status_code == 200, res.text
+            # fail to use a WORKFLOW task token on a non-WORKFLOW endpoint outside of Fence
+            # TODO something else than arborist
+            # url = f"{pytest.root_url}/authz/mapping"
+            # res = requests.get(url, headers={"Authorization": f"bearer {workflow_task_token}"})
+            # assert res.status_code == 401, res.text
+            # assert "token audience validation failed" in res.text
+
+            # succeed using a WORKFLOW task token on a WORKFLOW endpoint
+            res = requests.get(
+                f"http://127.0.0.1:{proxy_port}/ga4gh/tes/v1/tasks",
+                headers={"Authorization": f"bearer {workflow_task_token}"},
+            )
+            assert res.status_code == 200, res.text
+
+        with get_task_token("FOO") as (other_task_token, proxy_port):
+            # fail to use a non-WORKFLOW task token on a WORKFLOW endpoint
+            pass
+            # TODO uncomment once gen3-workflow is updated to reject non-WORKFLOW task tokens
+            # url = f"{pytest.root_url}/ga4gh/tes/v1/tasks"
+            # res = requests.get(url, headers={"Authorization": f"bearer {other_task_token}"})
+            # assert res.status_code == 401, res.text
+            # assert "token audience validation failed" in res.text
 
         # TODO succeed using a WORKFLOW task token on an Arborist endpoint, since Arborist should
         # accept all tokens for authz verification purposes
@@ -163,36 +181,46 @@ class TestTaskToken(object):
         reason="gen3-workflow service is not running on this environment",
     )
     @pytest.mark.gen3_workflow
-    def test_dpop_proxy(self): #, mock_auth_endpoint):
+    def test_dpop_proxy(self):
         """
         TODO
         """
-        # from gen3.auth import Gen3Auth
-        auth = Gen3Auth(refresh_token=pytest.api_keys["main_account"], endpoint=pytest.root_url)
-        # auth = self.gen3_workflow._get_auth_module()
-        # print('auth.endpoint', auth.endpoint)
-        # print(
-        #     "endpoint_from_token",
-        #     gen3.auth.endpoint_from_token(pytest.api_keys["main_account"]["api_key"]),
-        # )  # should be localhost.....
+        auth = Gen3Auth(
+            refresh_token=pytest.api_keys["main_account"], endpoint=pytest.root_url
+        )
+        with dpop_proxy_context(auth=auth, task_token_type="WORKFLOW") as (
+            task_token,
+            proxy_port,
+        ):
+            # url = f"{pytest.root_url}/user/credentials/api/access_token?task_token=WORKFLOW"
+            # res = requests.post(url, json={"api_key": pytest.api_keys[user]["api_key"]})
+            # assert res.status_code == 200, res.text
+            # print(res.json()["access_token"])
 
-        # resolved_tes_endpoint, resolved_s3_endpoint = resolve_service_endpoints(auth)
-        with dpop_proxy_context(auth=auth, task_token_type="WORKFLOW") as (task_token, proxy_port):
-            config_overrides = f"""process.container = 'public.ecr.aws/docker/library/alpine:latest'
-tes.endpoint = 'http://127.0.0.1:{proxy_port}/ga4gh/tes'
-tes.oauthToken = '{task_token}'
-aws.accessKey = '{task_token}'
-aws.client.endpoint = 'http://127.0.0.1:{proxy_port}/s3'"""
+            return
+            url = f"{pytest.root_url}/ga4gh/tes/v1/tasks"
+            res = requests.get(url, headers={"Authorization": f"bearer {task_token}"})
+            assert res.status_code == 401, res.text
+            assert res.json().get("error") == "dpop_required"
+
+            config_overrides = [
+                "process.container = 'quay.io/nextflow/bash'",  # TODO whitelist it
+                f"tes.endpoint = 'http://127.0.0.1:{proxy_port}/ga4gh/tes'",
+                f"tes.oauthToken = '{task_token}'",
+                f"aws.accessKey = '{task_token}'",
+                f"aws.client.endpoint = 'http://127.0.0.1:{proxy_port}/s3'",
+            ]
             with tempfile.NamedTemporaryFile(delete=True) as config_file:
-                config_file.write(config_overrides.encode())
+                config_file.write("\n".join(config_overrides).encode())
                 config_file.flush()
                 workflow_log = self.gen3_workflow.run_nextflow_workflow(
                     workflow_dir="test_data/gen3_workflow/",
-                    workflow_script="hello",
+                    workflow_script="hello",  # basic 4-task hello-world workflow
                     nextflow_config_files=["nextflow.config", config_file.name],
                     s3_working_directory=self.s3_storage_config.working_directory,
                     params={"gpu": "true", "run": "TEST_GPU"},
                 )
+
             logger.info(f"Workflow log:")
             completed_tasks = []
             for line in workflow_log.splitlines():
@@ -200,54 +228,12 @@ aws.client.endpoint = 'http://127.0.0.1:{proxy_port}/s3'"""
                 if "Task completed > TaskHandler" in line:
                     completed_tasks.append(nextflow_parse_completed_line(line))
 
-            for task_name, task in nextflow_parse_completed_tasks(completed_tasks).items():
-                print(task_name)
-
-
-
-            # env = os.environ.copy()
-            # env["GEN3_DPOP_BOUND_TASK_TOKEN"] = task_token
-            # env["GEN3_DPOP_PROXY_PORT"] = str(proxy_port)
-
-            # with contextlib.ExitStack() as stack:
-            #     args = list(nf_args)
-            #     if generate_config:
-            #         # The config outlives nothing: the directory goes away with the run.
-            #         directory = stack.enter_context(tempfile.TemporaryDirectory())
-            #         config_path = _write_proxy_config(
-            #             directory=directory,
-            #             proxy_port=proxy_port,
-            #             tes_endpoint=resolved_tes_endpoint,
-            #             s3_endpoint=resolved_s3_endpoint,
-            #         )
-            #         args += ["-c", config_path]
-            #         logger.info(f"Generated Nextflow config: {config_path}")
-
-            #     logger.info("*** Starting Nextflow run... ***")
-            #     try:
-            #         result = subprocess.run(
-            #             ["nextflow", "run"] + args, env=env, check=False
-            #         )
-            #     except FileNotFoundError:
-            #         logger.error(
-            #             "Could not find `nextflow` on your PATH. Install it and try again."
-            #         )
-            #         raise
-            #     logger.info("*** Completed Nextflow run! ***")
-            #     return result.returncode
-
-
-
-
-        # with dpop_proxy_context(auth=auth, task_token_type="WORKFLOW") as (
-        #     task_token,
-        #     proxy_port,
-        # ):
-        #     # Anything sent to 127.0.0.1:{proxy_port} is signed and forwarded.
-        #     print("task_token from proxy:", task_token)
-        #     url = f"{pytest.root_url}/ga4gh/tes/v1/tasks"
-        #     res = requests.get(
-        #         url, headers={"Authorization": f"bearer {task_token}"}
-        #     )
-        #     assert res.status_code == 200, res.text
-        #     # print(res.text)
+            for task_name, task in nextflow_parse_completed_tasks(
+                completed_tasks
+            ).items():
+                assert (
+                    task["status"] == "COMPLETED"
+                ), f"Task '{task_name}' failed with status: {task['status']}"
+                assert (
+                    task["exit_code"] == "0"
+                ), f"Task '{task_name}' returned exit code {task['exit_code']}"
